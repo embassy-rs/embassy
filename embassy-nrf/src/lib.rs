4e4241bf909b786a5950ac6d72a33a75d8ec982a
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-09-24 19:56:47 +0200

    remove `nrf` prefix in features, for consistency with nrf-hal

--- embassy-nrf/src/lib.rs
+++ embassy-nrf/src/lib.rs
@@ -4,37 +4,37 @@
 #![feature(type_alias_impl_trait)]
 
 #[cfg(not(any(
-    feature = "nrf52810",
-    feature = "nrf52811",
-    feature = "nrf52832",
-    feature = "nrf52833",
-    feature = "nrf52840",
+    feature = "52810",
+    feature = "52811",
+    feature = "52832",
+    feature = "52833",
+    feature = "52840",
 )))]
-compile_error!("No chip feature activated. You must activate exactly one of the following features: nrf52810, nrf52811, nrf52832, nrf52833, nrf52840");
+compile_error!("No chip feature activated. You must activate exactly one of the following features: 52810, 52811, 52832, 52833, 52840");
 
 #[cfg(any(
-    all(feature = "nrf52810", feature = "nrf52811"),
-    all(feature = "nrf52810", feature = "nrf52832"),
-    all(feature = "nrf52810", feature = "nrf52833"),
-    all(feature = "nrf52810", feature = "nrf52840"),
-    all(feature = "nrf52811", feature = "nrf52832"),
-    all(feature = "nrf52811", feature = "nrf52833"),
-    all(feature = "nrf52811", feature = "nrf52840"),
-    all(feature = "nrf52832", feature = "nrf52833"),
-    all(feature = "nrf52832", feature = "nrf52840"),
-    all(feature = "nrf52833", feature = "nrf52840"),
+    all(feature = "52810", feature = "52811"),
+    all(feature = "52810", feature = "52832"),
+    all(feature = "52810", feature = "52833"),
+    all(feature = "52810", feature = "52840"),
+    all(feature = "52811", feature = "52832"),
+    all(feature = "52811", feature = "52833"),
+    all(feature = "52811", feature = "52840"),
+    all(feature = "52832", feature = "52833"),
+    all(feature = "52832", feature = "52840"),
+    all(feature = "52833", feature = "52840"),
 ))]
-compile_error!("Multile chip features activated. You must activate exactly one of the following features: nrf52810, nrf52811, nrf52832, nrf52833, nrf52840");
+compile_error!("Multile chip features activated. You must activate exactly one of the following features: 52810, 52811, 52832, 52833, 52840");
 
-#[cfg(feature = "nrf52810")]
+#[cfg(feature = "52810")]
 pub use nrf52810_pac as pac;
-#[cfg(feature = "nrf52811")]
+#[cfg(feature = "52811")]
 pub use nrf52811_pac as pac;
-#[cfg(feature = "nrf52832")]
+#[cfg(feature = "52832")]
 pub use nrf52832_pac as pac;
-#[cfg(feature = "nrf52833")]
+#[cfg(feature = "52833")]
 pub use nrf52833_pac as pac;
-#[cfg(feature = "nrf52840")]
+#[cfg(feature = "52840")]
 pub use nrf52840_pac as pac;
 
 pub mod gpiote;
