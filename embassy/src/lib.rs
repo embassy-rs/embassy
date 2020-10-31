2c13e251849fd3e3cf06e2d5363c311ac880c77d
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-10-31 23:14:09 +0100

    Fix for latest nightly

--- embassy/src/lib.rs
+++ embassy/src/lib.rs
@@ -2,10 +2,11 @@
 #![feature(slice_fill)]
 #![feature(generic_associated_types)]
 #![feature(const_fn)]
+#![feature(const_fn_fn_ptr_basics)]
 
 pub mod executor;
 pub mod flash;
 pub mod io;
+pub mod rand;
 pub mod time;
 pub mod util;
-pub mod rand;
