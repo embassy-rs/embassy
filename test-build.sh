02af635e7b73676ef2529983cc5b1b0f890701ea
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2020-12-01 17:52:06 +0100

    Fix CI

--- test-build.sh
+++ test-build.sh
@@ -3,20 +3,20 @@
 set -euxo pipefail
 
 # examples
-(cd examples; cargo build --target thumbv7em-none-eabihf --bins)
+(cd examples; cargo build --target thumbv7em-none-eabi --bins)
 
 # embassy
-(cd embassy; cargo build --target thumbv7em-none-eabihf)
-(cd embassy; cargo build --target thumbv7em-none-eabihf --features log)
-(cd embassy; cargo build --target thumbv7em-none-eabihf --features defmt)
+(cd embassy; cargo build --target thumbv7em-none-eabi)
+(cd embassy; cargo build --target thumbv7em-none-eabi --features log)
+(cd embassy; cargo build --target thumbv7em-none-eabi --features defmt)
 
 # embassy-nrf
 (cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52810)
-#(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52811)  # nrf52811-hal doesn't exist yet
-(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52832)
-(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52833)
-(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52840)
+#(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52811)  # nrf52811-hal doesn't exist yet
+(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52832)
+(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52833)
+(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52840)
 
-(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52840,log)
-(cd embassy-nrf; cargo build --target thumbv7em-none-eabihf --features 52840,defmt)
+(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52840,log)
+(cd embassy-nrf; cargo build --target thumbv7em-none-eabi --features 52840,defmt)
 
