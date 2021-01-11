c91882a72c7bab61749b234c0e523fdbaee2c36b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-01-11 10:38:43 +0100

    Add CriticalSectionMutex, ThreadModeMutex.

--- embassy/src/util/mod.rs
+++ embassy/src/util/mod.rs
@@ -1,11 +1,13 @@
 mod drop_bomb;
 mod forever;
+mod mutex;
 mod portal;
 mod signal;
 mod waker;
 
 pub use drop_bomb::*;
 pub use forever::*;
+pub use mutex::*;
 pub use portal::*;
 pub use signal::*;
 pub use waker::*;
