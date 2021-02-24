f779ec4928c9163a6aa3850341819ecfea5ea2b7
fnafnio <fabiopungg@gmail.com>
2021-02-24 08:44:23 +0100

    update defmt and defmt-rtt to 0.2.0

--- embassy-stm32f4-examples/src/example_common.rs
+++ embassy-stm32f4-examples/src/example_common.rs
@@ -7,11 +7,11 @@ pub use defmt::*;
 
 use core::sync::atomic::{AtomicUsize, Ordering};
 
-#[defmt::timestamp]
-fn timestamp() -> u64 {
-    static COUNT: AtomicUsize = AtomicUsize::new(0);
-    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
-    let n = COUNT.load(Ordering::Relaxed);
-    COUNT.store(n + 1, Ordering::Relaxed);
-    n as u64
+defmt::timestamp! {"{=u64}", {
+        static COUNT: AtomicUsize = AtomicUsize::new(0);
+        // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+        let n = COUNT.load(Ordering::Relaxed);
+        COUNT.store(n + 1, Ordering::Relaxed);
+        n as u64
+    }
 }
