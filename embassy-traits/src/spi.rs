5df6e57949f07dd2ec95dadaf31877a1ab30e1cc
xoviat <xoviat@gmail.com>
2021-03-08 14:00:31 -0600

    update docs

--- embassy-traits/src/spi.rs
+++ embassy-traits/src/spi.rs
@@ -8,12 +8,12 @@ use core::future::Future;
 ///
 /// - It's the task of the user of this interface to manage the slave select lines
 ///
-/// - Due to how full duplex SPI works each `try_read` call must be preceded by a `try_send` call.
+/// - Due to how full duplex SPI works each `read` call must be preceded by a `write` call.
 ///
-/// - `try_read` calls only return the data received with the last `try_send` call.
+/// - `read` calls only return the data received with the last `write` call.
 /// Previously received data is discarded
 ///
-/// - Data is only guaranteed to be clocked out when the `try_read` call succeeds.
+/// - Data is only guaranteed to be clocked out when the `read` call succeeds.
 /// The slave select line shouldn't be released before that.
 ///
 /// - Some SPIs can work with 8-bit *and* 16-bit words. You can overload this trait with different
