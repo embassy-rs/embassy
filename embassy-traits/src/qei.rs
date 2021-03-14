a4bee1c1d2c8b252142bed7be8c16f9d77f3d1ff
xoviat <xoviat@users.noreply.github.com>
2021-03-14 17:41:59 -0500

    update docs

--- embassy-traits/src/qei.rs
+++ embassy-traits/src/qei.rs
@@ -6,6 +6,14 @@ use embedded_hal::Direction;
 pub trait WaitForRotate {
     type RotateFuture<'a>: Future<Output = Direction> + 'a;
 
+    /// Wait for a specified number of rotations, in ticks, either up or down.
+    ///
+    /// Return Direction::Upcounting if the high bound is reached.
+    /// Return Direction::Downcounting if the low bound is reached.
+    ///
+    /// Number of ticks is encoder dependent. As an example, if we connect
+    /// the Bourns PEC11H-4120F-S0020, we have 20 ticks per full rotation.
+    /// Other encoders may vary.
     fn wait_for_rotate<'a>(
         self: Pin<&'a mut Self>,
         count_down: u16,
