f248bb17ad2840a87a6e4858e72d52fd067609ec
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-03-18 01:42:28 +0100

    Allow unused stuff in fmt.rs

--- embassy-extras/src/fmt.rs
+++ embassy-extras/src/fmt.rs
@@ -1,5 +1,6 @@
 #![macro_use]
 #![allow(clippy::module_inception)]
+#![allow(unused)]
 
 #[cfg(all(feature = "defmt", feature = "log"))]
 compile_error!("You may not enable both `defmt` and `log` features.");
