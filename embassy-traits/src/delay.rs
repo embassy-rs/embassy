8fbe83ac26185482069ee7b9049879cd9be01462
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-03-21 18:30:03 -0700

    Added remainder of the docs for embassy::time's public interface.

--- embassy-traits/src/delay.rs
+++ embassy-traits/src/delay.rs
@@ -4,6 +4,9 @@ use core::pin::Pin;
 pub trait Delay {
     type DelayFuture<'a>: Future<Output = ()> + 'a;
 
+    /// Future that completes after now + millis
     fn delay_ms<'a>(self: Pin<&'a mut Self>, millis: u64) -> Self::DelayFuture<'a>;
+
+    /// Future that completes after now + micros
     fn delay_us<'a>(self: Pin<&'a mut Self>, micros: u64) -> Self::DelayFuture<'a>;
 }
