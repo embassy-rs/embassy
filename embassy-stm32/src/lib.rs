8d014f7955b3c068ef4c15d79a821306e7afa5e0
xoviat <xoviat@users.noreply.github.com>
2021-03-27 21:42:26 -0500

    restrict usb to stm32f4

--- embassy-stm32/src/lib.rs
+++ embassy-stm32/src/lib.rs
@@ -75,4 +75,23 @@ pub mod can;
 ))]
 pub mod rtc;
 
+#[cfg(any(
+    feature = "stm32f401",
+    feature = "stm32f405",
+    feature = "stm32f407",
+    feature = "stm32f410",
+    feature = "stm32f411",
+    feature = "stm32f412",
+    feature = "stm32f413",
+    feature = "stm32f415",
+    feature = "stm32f417",
+    feature = "stm32f423",
+    feature = "stm32f427",
+    feature = "stm32f429",
+    feature = "stm32f437",
+    feature = "stm32f439",
+    feature = "stm32f446",
+    feature = "stm32f469",
+    feature = "stm32f479",
+))]
 unsafe impl embassy_extras::usb::USBInterrupt for interrupt::OTG_FS {}
