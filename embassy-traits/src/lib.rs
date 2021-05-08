6c3299c82fe6c61c0b521edfc3ab2a3306702501
Rukai <rubickent@gmail.com>
2021-05-08 13:04:25 +1000

    Fix build on the latest nightly

--- embassy-traits/src/lib.rs
+++ embassy-traits/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(generic_associated_types)]
-#![feature(const_fn)]
 #![feature(const_fn_fn_ptr_basics)]
 #![feature(const_option)]
 #![allow(incomplete_features)]
