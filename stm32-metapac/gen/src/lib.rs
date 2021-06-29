2a25de3d3e16ebf8f21323ce016fe2ecf4d21629
Bob McWhirter <bmcwhirt@redhat.com>
2021-06-29 12:55:15 -0400

    Make the metapac gen enr/rst missing regs non-fatal to the build. Should be solved in a separate effort.

--- stm32-metapac/gen/src/lib.rs
+++ stm32-metapac/gen/src/lib.rs
@@ -431,13 +431,13 @@ pub fn gen(options: Options) {
                                 ]);
                             }
                             (None, Some(_)) => {
-                                panic!("Unable to find enable register for {}", name)
+                                print!("Unable to find enable register for {}", name)
                             }
                             (Some(_), None) => {
-                                panic!("Unable to find reset register for {}", name)
+                                print!("Unable to find reset register for {}", name)
                             }
                             (None, None) => {
-                                panic!("Unable to find enable and reset register for {}", name)
+                                print!("Unable to find enable and reset register for {}", name)
                             }
                         }
                     }
