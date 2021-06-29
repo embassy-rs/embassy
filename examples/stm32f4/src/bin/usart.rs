d5fb558005c560d7cbc9c1276e65d7d6a64452f9
Bob McWhirter <bmcwhirt@redhat.com>
2021-06-29 13:08:18 -0400

    Fix non-DMA USART example.

--- examples/stm32f4/src/bin/usart.rs
+++ examples/stm32f4/src/bin/usart.rs
@@ -23,7 +23,7 @@ async fn main_task() {
     let p = embassy_stm32::init(Default::default());
 
     let config = Config::default();
-    let mut usart = Uart::new(p.USART3, p.PD9, p.PD8, config, 16_000_000);
+    let mut usart = Uart::new(p.USART3, p.PD9, p.PD8, config);
 
     usart.bwrite_all(b"Hello Embassy World!\r\n").unwrap();
     info!("wrote Hello, starting echo");
