a88f0028ef2f1c57a66d7c3ed587b1ff2129bec2
Bob McWhirter <bmcwhirt@redhat.com>
2021-07-14 14:14:14 -0400

    First shot at async dma usart for stm32.

--- embassy-stm32/src/dma_traits.rs
+++ embassy-stm32/src/dma_traits.rs
@@ -1,4 +1,5 @@
 use core::future::Future;
+use embassy::util::Unborrow;
 
 pub trait WriteDma<T> {
     type WriteDmaFuture<'a>: Future<Output = ()> + 'a
@@ -18,4 +19,18 @@ pub trait ReadDma<T> {
     fn transfer<'a>(&'a mut self, src: *const u8, buf: &'a mut [u8]) -> Self::ReadDmaFuture<'a>
     where
         T: 'a;
+}
+
+pub trait NoDmaMarker {}
+
+pub struct NoDma;
+
+impl NoDmaMarker for NoDma {}
+
+unsafe impl Unborrow for NoDma {
+    type Target = NoDma;
+
+    unsafe fn unborrow(self) -> Self::Target {
+        self
+    }
 }
