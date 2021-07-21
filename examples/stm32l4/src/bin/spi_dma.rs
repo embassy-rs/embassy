e269971597f45ac4138b9097538fbdd7dc7be588
Bob McWhirter <bmcwhirt@redhat.com>
2021-07-21 11:20:03 -0400

    Fix extraneous `mut` warnings in L4 example.

--- examples/stm32l4/src/bin/spi_dma.rs
+++ examples/stm32l4/src/bin/spi_dma.rs
@@ -39,11 +39,11 @@ async fn main_task() {
 
     // These are the pins for the Inventek eS-Wifi SPI Wifi Adapter.
 
-    let mut boot = Output::new(p.PB12, Level::Low, Speed::VeryHigh);
-    let mut wake = Output::new(p.PB13, Level::Low, Speed::VeryHigh);
+    let boot = Output::new(p.PB12, Level::Low, Speed::VeryHigh);
+    let wake = Output::new(p.PB13, Level::Low, Speed::VeryHigh);
     let mut reset = Output::new(p.PE8, Level::Low, Speed::VeryHigh);
     let mut cs = Output::new(p.PE0, Level::High, Speed::VeryHigh);
-    let mut ready = Input::new(p.PE1, Pull::Up);
+    let ready = Input::new(p.PE1, Pull::Up);
 
     cortex_m::asm::delay(100_000);
     reset.set_high();
