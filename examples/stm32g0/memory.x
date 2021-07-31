ee841499ee8ee4d073930b2645eb70a0e652b383
Ben Gamari <ben@smart-cactus.org>
2021-07-31 10:01:32 -0400

    Add STM32G0 examples

---
+++ examples/stm32g0/memory.x
@@ -0,0 +1,7 @@
+MEMORY
+{
+  /* NOTE 1 K = 1 KiBi = 1024 bytes */
+  /* These values correspond to the STM32G071C8 */
+  FLASH : ORIGIN = 0x08000000, LENGTH = 64K
+  RAM : ORIGIN = 0x20000000, LENGTH = 36K
+}
---
+++ examples/stm32g0/src/bin/button.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+#![feature(trait_alias)]
+#![feature(type_alias_impl_trait)]
+#![allow(incomplete_features)]
+
+#[path = "../example_common.rs"]
+mod example_common;
+use cortex_m_rt::entry;
+use embassy_stm32::gpio::{Input, Pull};
+use embedded_hal::digital::v2::InputPin;
+use example_common::*;
+
+#[entry]
+fn main() -> ! {
+    info!("Hello World!");
+
+    let p = embassy_stm32::init(Default::default());
+
+    let button = Input::new(p.PC13, Pull::Up);
+
+    loop {
+        if unwrap!(button.is_high()) {
+            info!("high");
+        } else {
+            info!("low");
+        }
+    }
+}
