ec51880e28d48269ae1ce8b037305f4f7f182c84
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2021-08-19 22:17:17 +0200

    stm32/exti: unify all versions into single impl

---
+++ examples/stm32wb55/src/bin/button_exti.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+#![feature(trait_alias)]
+#![feature(type_alias_impl_trait)]
+#![allow(incomplete_features)]
+
+#[path = "../example_common.rs"]
+mod example_common;
+use embassy::executor::Spawner;
+use embassy_stm32::dbgmcu::Dbgmcu;
+use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::gpio::{Input, Pull};
+use embassy_stm32::Peripherals;
+use embassy_traits::gpio::{WaitForFallingEdge, WaitForRisingEdge};
+use example_common::*;
+
+#[embassy::main]
+async fn main(_spawner: Spawner, p: Peripherals) {
+    info!("Hello World!");
+
+    unsafe {
+        Dbgmcu::enable_all();
+    }
+
+    let button = Input::new(p.PC4, Pull::Up);
+    let mut button = ExtiInput::new(button, p.EXTI4);
+
+    info!("Press the USER button...");
+
+    loop {
+        button.wait_for_falling_edge().await;
+        info!("Pressed!");
+        button.wait_for_rising_edge().await;
+        info!("Released!");
+    }
+}
