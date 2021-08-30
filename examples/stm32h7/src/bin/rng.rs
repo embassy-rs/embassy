fd7a76c59e8a190781aea476d199bda826dd5031
Bob McWhirter <bmcwhirt@redhat.com>
2021-08-30 09:57:31 -0400

    Formatting. Always formatting.

--- examples/stm32h7/src/bin/rng.rs
+++ examples/stm32h7/src/bin/rng.rs
@@ -8,12 +8,12 @@
 mod example_common;
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
+use embassy::traits::rng::Random as _;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::rng::Random;
 use embassy_stm32::Peripherals;
 use embedded_hal::digital::v2::OutputPin;
 use example_common::*;
-use embassy_stm32::rng::Random;
-use embassy::traits::rng::Random as _;
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
@@ -24,11 +24,11 @@ async fn main(_spawner: Spawner, p: Peripherals) {
     let mut rng = Random::new(p.RNG);
 
     loop {
-        info!("high {}", unwrap!(rng.next(16).await) );
+        info!("high {}", unwrap!(rng.next(16).await));
         unwrap!(led.set_high());
         Timer::after(Duration::from_millis(500)).await;
 
-        info!("low {}", unwrap!(rng.next(16).await) );
+        info!("low {}", unwrap!(rng.next(16).await));
         unwrap!(led.set_low());
         Timer::after(Duration::from_millis(500)).await;
     }
