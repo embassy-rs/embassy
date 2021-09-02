4dccda085f6730bc367faa58bec72fce53a49762
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-09-02 11:19:54 +0200

    Add missing files for G0

--- embassy-stm32/src/pwr/mod.rs
+++ embassy-stm32/src/pwr/mod.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(any(pwr_h7, pwr_h7smps), path = "h7.rs")]
 #[cfg_attr(pwr_f4, path = "f4.rs")]
 #[cfg_attr(pwr_wl5, path = "wl5.rs")]
+#[cfg_attr(pwr_g0, path = "g0.rs")]
 mod _version;
 
 pub use _version::*;
