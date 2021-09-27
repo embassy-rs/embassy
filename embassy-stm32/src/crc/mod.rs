e67af514e921b3b28b4c20a88554c4d7cbef7951
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-09-26 19:15:54 -0700

    Fix v2/3 module paths

--- embassy-stm32/src/crc/mod.rs
+++ embassy-stm32/src/crc/mod.rs
@@ -1,9 +1,6 @@
-use crate::peripherals::CRC;
-
-
-#[cfg_attr(crc_v2, path = "v1.rs")]
-#[cfg_attr(crc_v1, path = "v2.rs")]
-#[cfg_attr(crc_v3, path = "v3.rs")]
+#[cfg_attr(crc_v2, path = "v2.rs")]
+#[cfg_attr(crc_v1, path = "v1.rs")]
+#[cfg_attr(crc_v3, path = "v2.rs")]
 mod _version;
 
 pub use _version::Crc;
