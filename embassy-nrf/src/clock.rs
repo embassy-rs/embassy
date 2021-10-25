b204aa89fe371478dbf4f074e2a9052cf7dba88f
Hannes Hochreiner <hannes@hochreiner.net>
2021-10-25 21:38:44 +0200

    implement hfclk

--- embassy-nrf/src/clock.rs
+++ embassy-nrf/src/clock.rs
@@ -20,39 +20,52 @@ use futures::future::poll_fn;
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum Error {
-    Error,
+    HfClkError,
 }
 
+#[derive(Clone, Copy)]
 pub enum LfClkSource {
     Rc,
     Xtal,
     Sync,
 }
 
+#[derive(Clone, Copy)]
 pub enum HfClkSource {
     Rc,
     Xtal,
 }
 
 #[non_exhaustive]
-pub struct Config {
+pub struct LfClockConfig {
     /// Low-frequency clock source
-    lf_clk_source: LfClkSource,
+    pub source: LfClkSource,
     /// Low-frequency clock bypass
-    lf_clk_bypass: bool,
+    pub bypass: bool,
     /// Low-frequency clock external
-    lf_clk_external: bool,
-    /// High-frequency clock source
-    hf_clk_source: HfClkSource,
+    pub external: bool,
 }
 
-impl Default for Config {
+impl Default for LfClockConfig {
     fn default() -> Self {
         Self {
-            lf_clk_source: LfClkSource::Rc,
-            lf_clk_bypass: false,
-            lf_clk_external: false,
-            hf_clk_source: HfClkSource::Rc,
+            source: LfClkSource::Rc,
+            bypass: false,
+            external: false,
+        }
+    }
+}
+
+#[non_exhaustive]
+pub struct HfClockConfig {
+    /// High-frequency clock source
+    pub source: HfClkSource,
+}
+
+impl Default for HfClockConfig {
+    fn default() -> Self {
+        Self {
+            source: HfClkSource::Rc,
         }
     }
 }
@@ -66,7 +79,6 @@ impl<'d, T: Instance> Clock<'d, T> {
     pub fn new(
         _clock: impl Unborrow<Target = T> + 'd,
         irq: impl Unborrow<Target = T::Interrupt> + 'd,
-        config: Config,
     ) -> Self {
         unborrow!(irq);
 
@@ -84,6 +96,35 @@ impl<'d, T: Instance> Clock<'d, T> {
         }
     }
 
+    pub fn set_hf_clock_config<'a>(&mut self, config: &'a HfClockConfig) -> impl Future<Output = Result<(), Error>> + 'a {
+        async move {
+            let r = T::regs();
+
+            match (config.source, r.hfclkstat.read().bits() & 0x0000_0001) {
+                // source is xtal, but rc is requested => stop hfxo
+                (HfClkSource::Rc, 1) => {
+                    r.tasks_hfclkstop.write(|w| w.tasks_hfclkstop().set_bit());
+                    Ok(())
+                },
+                // source is rc, but xtal is requested => start hfxo
+                (HfClkSource::Xtal, 0) => {
+                    // enable "disabled" interrupt
+                    r.intenset.write(|w| w.hfclkstarted().bit(true));
+
+                    // start hfxo
+                    r.tasks_hfclkstart.write(|w| w.tasks_hfclkstart().set_bit());
+
+                    // Wait for 'started' event.
+                    poll_fn(Self::wait_for_started_event).await;
+
+                    // r.hfclkstat is not immediately updated, so we can´t check it
+                    Ok(())
+                },
+                _ => Ok(())
+            }
+        }
+    }
+
     fn on_interrupt(_: *mut ()) {
         let r = T::regs();
         let s = T::state();
@@ -98,11 +139,12 @@ impl<'d, T: Instance> Clock<'d, T> {
         }
     }
 
-    fn wait_for_disabled_event(cx: &mut core::task::Context) -> Poll<()> {
+    fn wait_for_started_event(cx: &mut core::task::Context) -> Poll<()> {
         let r = T::regs();
         let s = T::state();
 
         s.end_waker.register(cx.waker());
+
         if r.events_lfclkstarted.read().bits() != 0 {
             r.events_lfclkstarted.reset();
 
@@ -119,19 +161,19 @@ impl<'d, T: Instance> Clock<'d, T> {
     }
 }
 
-impl<'a, T: Instance> Drop for Clock<'a, T> {
-    fn drop(&mut self) {
-        info!("clock drop");
+// impl<'a, T: Instance> Drop for Clock<'a, T> {
+//     fn drop(&mut self) {
+//         info!("clock drop");
 
-        // TODO when implementing async here, check for abort
+//         // TODO when implementing async here, check for abort
 
-        // disable TODO check
-        // let r = T::regs();
-        // r.power.write(|w| w.power().disabled());
+//         // disable TODO check
+//         // let r = T::regs();
+//         // r.power.write(|w| w.power().disabled());
 
-        info!("clock drop: done");
-    }
-}
+//         info!("clock drop: done");
+//     }
+// }
 
 pub(crate) mod sealed {
     use super::*;
