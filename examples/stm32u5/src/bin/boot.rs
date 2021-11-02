9deafa8bab6c2ba4fef91a964defdf7f24c52d05
Bob McWhirter <bmcwhirt@redhat.com>
2021-11-02 15:56:04 -0400

    Remove unused imports.

--- examples/stm32u5/src/bin/boot.rs
+++ examples/stm32u5/src/bin/boot.rs
@@ -4,8 +4,6 @@
 
 #[path = "../example_common.rs"]
 mod example_common;
-use embassy_stm32::gpio::{Input, Pull};
-use embedded_hal::digital::v2::InputPin;
 use example_common::*;
 
 #[cortex_m_rt::entry]
@@ -13,16 +11,4 @@ fn main() -> ! {
     info!("Hello World!");
 
     loop {}
-
-    //let p = embassy_stm32::init(Default::default());
-
-    //let button = Input::new(p.PC13, Pull::Up);
-
-    //loop {
-    //if unwrap!(button.is_high()) {
-    //info!("high");
-    //} else {
-    //info!("low");
-    //}
-    //}
 }
