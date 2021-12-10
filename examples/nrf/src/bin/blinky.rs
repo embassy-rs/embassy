e93f2679b1edbedd83e09fbc3b7a07dbf1ef80a4
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-12-10 12:27:44 +0100

    More content

--- examples/nrf/src/bin/blinky.rs
+++ examples/nrf/src/bin/blinky.rs
@@ -8,18 +8,21 @@ mod example_common;
 use defmt::unwrap;
 use embassy::executor::Spawner;
 use embassy::time::{Duration, Timer};
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_nrf::Peripherals;
+use embassy_nrf::{Peripherals, peripherals::P0_13, gpio::{Level, Output, OutputDrive};
 use embedded_hal::digital::v2::OutputPin;
+
+#[embassh::task]
+async fn blinker(led: Output<'static, P0_13>, interval: Duration) {
+    loop {
+        unwrap!(led.set_high());
+        Timer::after(interval).await;
+        unwrap!(led.set_low());
+        Timer::after(interval).await;
+    }
+}
 
 #[embassy::main]
 async fn main(_spawner: Spawner, p: Peripherals) {
     let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
-
-    loop {
-        unwrap!(led.set_high());
-        Timer::after(Duration::from_millis(300)).await;
-        unwrap!(led.set_low());
-        Timer::after(Duration::from_millis(300)).await;
-    }
+    unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
 }
