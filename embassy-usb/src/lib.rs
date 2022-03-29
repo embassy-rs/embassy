c016d77e4fab17c0b5389de868e2ea25ff02c0cf
alexmoon <alex.r.moon@gmail.com>
2022-03-28 20:10:13 -0400

    Switch to ControlHandler owned bufs for control_in()

--- embassy-usb/src/lib.rs
+++ embassy-usb/src/lib.rs
@@ -284,13 +284,10 @@ impl<'d, D: Driver<'d>> UsbDevice<'d, D> {
                     .find(|(i, _)| req.index == *i as _)
                     .map(|(_, h)| h);
                 match handler {
-                    Some(handler) => {
-                        let mut buf = [0; 128];
-                        match handler.control_in(req, &mut buf) {
-                            InResponse::Accepted(len) => self.control.accept_in(&buf[..len]).await,
-                            InResponse::Rejected => self.control.reject(),
-                        }
-                    }
+                    Some(handler) => match handler.control_in(req) {
+                        InResponse::Accepted(data) => self.control.accept_in(data).await,
+                        InResponse::Rejected => self.control.reject(),
+                    },
                     None => self.control.reject(),
                 }
             }
