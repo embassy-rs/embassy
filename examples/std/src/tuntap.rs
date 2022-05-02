a5f5c3a844561d56c91513f36a1077038d8a0336
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-05-02 16:15:05 +0200

    net: add functions to get current Eth and IP config

--- examples/std/src/tuntap.rs
+++ examples/std/src/tuntap.rs
@@ -219,7 +219,7 @@ impl Device for TunTapDevice {
         LinkState::Up
     }
 
-    fn ethernet_address(&mut self) -> [u8; 6] {
+    fn ethernet_address(&self) -> [u8; 6] {
         [0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
     }
 }
