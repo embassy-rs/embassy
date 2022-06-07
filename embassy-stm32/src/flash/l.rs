7d64f5cda7226f7a30a38da68d9d4e2a31a21eaa
Ulf Lilleengen <lulf@redhat.com>
2022-06-07 15:59:22 +0200

    Use correct index and bank

--- embassy-stm32/src/flash/l.rs
+++ embassy-stm32/src/flash/l.rs
@@ -77,7 +77,14 @@ pub(crate) unsafe fn blocking_erase(from: u32, to: u32) -> Result<(), Error> {
 
         #[cfg(any(flash_wl, flash_wb, flash_l4))]
         {
-            let idx = page / super::ERASE_SIZE as u32;
+            let idx = (page - super::FLASH_BASE as u32) / super::ERASE_SIZE as u32;
+
+            #[cfg(flash_l4)]
+            let (idx, bank) = if idx > 255 {
+                (idx - 256, true)
+            } else {
+                (idx, false)
+            };
 
             pac::FLASH.cr().modify(|w| {
                 w.set_per(true);
@@ -86,6 +93,8 @@ pub(crate) unsafe fn blocking_erase(from: u32, to: u32) -> Result<(), Error> {
                 w.set_strt(true);
                 #[cfg(any(flash_l4))]
                 w.set_start(true);
+                #[cfg(any(flash_l4))]
+                w.set_bker(bank);
             });
         }
 
