c9b58561538b5fb3e5702744c9f5653d039f8c98
Henrik Alsér <henrik@mindbite.se>
2022-07-10 00:58:05 +0200

    Features only on nightly

--- embassy-embedded-hal/src/lib.rs
+++ embassy-embedded-hal/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![feature(generic_associated_types)]
-#![feature(type_alias_impl_trait)]
+#![cfg_attr(feature = "nightly", feature(generic_associated_types, type_alias_impl_trait))]
 
 #[cfg(feature = "nightly")]
 pub mod adapter;
