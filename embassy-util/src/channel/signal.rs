17b9eb5f86fe35a0951d90752fd99ca2d63783cb
Bob McWhirter <bmcwhirt@redhat.com>
2022-08-15 11:07:46 -0400

    Update signal.rs
    Allow `poll_wait` to be public so higher-order futures around Signal can be built.

--- embassy-util/src/channel/signal.rs
+++ embassy-util/src/channel/signal.rs
@@ -69,7 +69,7 @@ impl<T: Send> Signal<T> {
         })
     }
 
-    fn poll_wait(&self, cx: &mut Context<'_>) -> Poll<T> {
+    pub fn poll_wait(&self, cx: &mut Context<'_>) -> Poll<T> {
         critical_section::with(|_| unsafe {
             let state = &mut *self.state.get();
             match state {
