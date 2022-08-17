bd0aaec6242c86d126fdb3784ae603cbf329957b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-08-17 15:03:12 +0200

    macros: remove embassy_prefix attr.
    This was used in the past for reexporting the macros from drogue-device,
    which is no longer using it.
    
    Also, it is a pain to support, so we don't want it.

--- embassy-macros/src/macros/main.rs
+++ embassy-macros/src/macros/main.rs
@@ -3,28 +3,16 @@ use proc_macro2::TokenStream;
 use quote::quote;
 
 use crate::util::ctxt::Ctxt;
-use crate::util::path::ModulePrefix;
-
-#[cfg(feature = "stm32")]
-const HAL: Option<&str> = Some("embassy_stm32");
-#[cfg(feature = "nrf")]
-const HAL: Option<&str> = Some("embassy_nrf");
-#[cfg(feature = "rp")]
-const HAL: Option<&str> = Some("embassy_rp");
-#[cfg(not(any(feature = "stm32", feature = "nrf", feature = "rp")))]
-const HAL: Option<&str> = None;
 
 #[derive(Debug, FromMeta)]
 struct Args {
-    #[darling(default)]
-    embassy_prefix: ModulePrefix,
-
     #[allow(unused)]
     #[darling(default)]
     config: Option<syn::LitStr>,
 }
 
 pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, TokenStream> {
+    #[allow(unused_variables)]
     let args = Args::from_list(&args).map_err(|e| e.write_errors())?;
 
     let fargs = f.sig.inputs.clone();
@@ -38,26 +26,32 @@ pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, Toke
         ctxt.error_spanned_by(&f.sig, "task functions must not be generic");
     }
 
-    if HAL.is_some() && fargs.len() != 2 {
+    #[cfg(feature = "stm32")]
+    let hal = Some(quote!(::embassy_stm32));
+    #[cfg(feature = "nrf")]
+    let hal = Some(quote!(::embassy_nrf));
+    #[cfg(feature = "rp")]
+    let hal = Some(quote!(::embassy_rp));
+    #[cfg(not(any(feature = "stm32", feature = "nrf", feature = "rp")))]
+    let hal: Option<TokenStream> = None;
+
+    if hal.is_some() && fargs.len() != 2 {
         ctxt.error_spanned_by(&f.sig, "main function must have 2 arguments");
     }
-    if HAL.is_none() && fargs.len() != 1 {
+    if hal.is_none() && fargs.len() != 1 {
         ctxt.error_spanned_by(&f.sig, "main function must have 1 argument");
     }
 
     ctxt.check()?;
 
-    let embassy_prefix = args.embassy_prefix;
-    let embassy_prefix_lit = embassy_prefix.literal();
-    let embassy_path = embassy_prefix.append("embassy_executor").path();
     let f_body = f.block;
 
     #[cfg(feature = "wasm")]
     let main = quote! {
         #[wasm_bindgen::prelude::wasm_bindgen(start)]
         pub fn main() -> Result<(), wasm_bindgen::JsValue> {
-            static EXECUTOR: ::embassy_util::Forever<#embassy_path::executor::Executor> = ::embassy_util::Forever::new();
-            let executor = EXECUTOR.put(#embassy_path::executor::Executor::new());
+            static EXECUTOR: ::embassy_util::Forever<::embassy_executor::executor::Executor> = ::embassy_util::Forever::new();
+            let executor = EXECUTOR.put(::embassy_executor::executor::Executor::new());
 
             executor.start(|spawner| {
                 spawner.spawn(__embassy_main(spawner)).unwrap();
@@ -70,7 +64,7 @@ pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, Toke
     #[cfg(all(feature = "std", not(feature = "wasm")))]
     let main = quote! {
         fn main() -> ! {
-            let mut executor = #embassy_path::executor::Executor::new();
+            let mut executor = ::embassy_executor::executor::Executor::new();
             let executor = unsafe { __make_static(&mut executor) };
 
             executor.run(|spawner| {
@@ -87,16 +81,13 @@ pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, Toke
             })
         });
 
-        let (hal_setup, peris_arg) = match HAL {
-            Some(hal) => {
-                let embassy_hal_path = embassy_prefix.append(hal).path();
-                (
-                    quote!(
-                        let p = #embassy_hal_path::init(#config);
-                    ),
-                    quote!(p),
-                )
-            }
+        let (hal_setup, peris_arg) = match hal {
+            Some(hal) => (
+                quote!(
+                    let p = #hal::init(#config);
+                ),
+                quote!(p),
+            ),
             None => (quote!(), quote!()),
         };
 
@@ -105,7 +96,7 @@ pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, Toke
             fn main() -> ! {
                 #hal_setup
 
-                let mut executor = #embassy_path::executor::Executor::new();
+                let mut executor = ::embassy_executor::executor::Executor::new();
                 let executor = unsafe { __make_static(&mut executor) };
 
                 executor.run(|spawner| {
@@ -116,7 +107,7 @@ pub fn run(args: syn::AttributeArgs, f: syn::ItemFn) -> Result<TokenStream, Toke
     };
 
     let result = quote! {
-        #[#embassy_path::task(embassy_prefix = #embassy_prefix_lit)]
+        #[::embassy_executor::task()]
         async fn __embassy_main(#fargs) {
             #f_body
         }
