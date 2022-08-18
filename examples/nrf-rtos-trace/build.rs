2edf532f8d8ce048137990bf74b07759428ed7c1
Quentin Smith <quentin@mit.edu>
2022-08-18 01:38:58 -0400

    Move rtos-trace example to a separate project to simplify Cargo.toml

--- examples/nrf-rtos-trace/build.rs
+++ examples/nrf-rtos-trace/build.rs
@@ -31,5 +31,6 @@ fn main() {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
+    #[cfg(feature = "defmt")]
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
