764ee3b72cc8770f7c398ba1aee41fbd34f07764
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-08-28 22:51:52 +0200

    futures: add block_on

--- embassy-futures/src/lib.rs
+++ embassy-futures/src/lib.rs
@@ -5,8 +5,10 @@
 // This mod MUST go first, so that the others see its macros.
 pub(crate) mod fmt;
 
+mod block_on;
 mod select;
 mod yield_now;
 
+pub use block_on::*;
 pub use select::*;
 pub use yield_now::*;
