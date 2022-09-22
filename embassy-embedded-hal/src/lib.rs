897b72c872183221e088611aa6f30989800afd2b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-09-22 16:28:56 +0200

    Update Rust nightly.
    Removes feature(generic_associated_types)

--- embassy-embedded-hal/src/lib.rs
+++ embassy-embedded-hal/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(feature = "nightly", feature(generic_associated_types, type_alias_impl_trait))]
+#![cfg_attr(feature = "nightly", feature(type_alias_impl_trait))]
 #![warn(missing_docs)]
 
 //! Utilities to use `embedded-hal` traits with Embassy.
