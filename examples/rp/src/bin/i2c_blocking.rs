c96581879cf054d9e4fec9272e5cecb165844cbb
Jeremy Fitzhardinge <jeremy@goop.org>
2022-10-01 01:33:36 -0700

    update embedded-hal api
    Also pin to alpha.9 since its a breaking change

--- examples/rp/src/bin/i2c_blocking.rs
+++ examples/rp/src/bin/i2c_blocking.rs
@@ -6,7 +6,7 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::i2c::{self, Config};
 use embassy_time::{Duration, Timer};
-use embedded_hal_1::i2c::blocking::I2c;
+use embedded_hal_1::i2c::I2c;
 use {defmt_rtt as _, panic_probe as _};
 
 #[allow(dead_code)]
