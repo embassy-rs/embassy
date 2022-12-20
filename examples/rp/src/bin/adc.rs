5ae91ed3b64a086b15a949477a5762a3876307c5
Aaron Tsui <aaron.tsui@outlook.com>
2022-12-20 14:59:49 +0800

    cargo fmt

--- examples/rp/src/bin/adc.rs
+++ examples/rp/src/bin/adc.rs
@@ -34,5 +34,5 @@ async fn main(_spawner: Spawner) {
 
 fn convert_to_celsius(raw_temp: u16) -> f32 {
     // According to chapter 4.9.5. Temperature Sensor in RP2040 datasheet
-    27.0 - (raw_temp as f32 * 3.3 / 4096.0 -0.706)/0.001721 as f32
+    27.0 - (raw_temp as f32 * 3.3 / 4096.0 - 0.706) / 0.001721 as f32
 }
