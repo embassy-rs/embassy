c4cbb89fcd60375c4982a61fc3d9e5183f97d748
Samuel Tardieu <sam@rfc1149.net>
2023-02-02 12:51:47 +0100

    LoRa/SX126x: adjust Rx window offset and duration
    Those timings open Rx time windows covering 99.7% of the one expected
    by the antenna while allowing 3ms for the Rx subsystem to start listening.

--- embassy-lora/src/sx126x/mod.rs
+++ embassy-lora/src/sx126x/mod.rs
@@ -55,10 +55,10 @@ where
     BUS: Error + Format + 'static,
 {
     fn get_rx_window_offset_ms(&self) -> i32 {
-        -500
+        -3
     }
     fn get_rx_window_duration_ms(&self) -> u32 {
-        800
+        1003
     }
 }
 
