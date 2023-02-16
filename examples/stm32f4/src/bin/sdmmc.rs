ab7e26a777501746088dddcb8afd60513919a6e0
chemicstry <chemicstry@gmail.com>
2023-02-17 01:30:23 +0200

    Fix sdmmc v1 writes

--- examples/stm32f4/src/bin/sdmmc.rs
+++ examples/stm32f4/src/bin/sdmmc.rs
@@ -9,6 +9,9 @@ use embassy_stm32::time::mhz;
 use embassy_stm32::{interrupt, Config};
 use {defmt_rtt as _, panic_probe as _};
 
+#[repr(align(4))]
+struct AlignedBuffer([u8; 512]);
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let mut config = Config::default();
@@ -34,11 +37,24 @@ async fn main(_spawner: Spawner) -> ! {
     // Should print 400kHz for initialization
     info!("Configured clock: {}", sdmmc.clock().0);
 
-    unwrap!(sdmmc.init_card(mhz(25)).await);
+    unwrap!(sdmmc.init_card(mhz(2)).await);
 
     let card = unwrap!(sdmmc.card());
 
     info!("Card: {:#?}", Debug2Format(card));
+    info!("Clock: {}", sdmmc.clock());
+
+    let mut buf = AlignedBuffer([0u8; 512]);
+
+    info!("read");
+    sdmmc.read_block(512, &mut buf.0).await.unwrap();
+    info!("read done: {:?}, {:?}", buf.0[..10], buf.0[502..]);
+
+    let buf = AlignedBuffer([6u8; 512]);
+
+    info!("writing");
+    sdmmc.write_block(0, &buf.0).await.unwrap();
+    info!("Write done!");
 
     loop {}
 }
