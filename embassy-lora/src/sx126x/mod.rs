7783e0ebb1a3870f3b180c87c1acd2807439f976
Caleb Jamison <caleb@cbjamo.com>
2023-02-17 07:43:19 -0500

    Change timing window to match values found experimentally.

--- embassy-lora/src/sx126x/mod.rs
+++ embassy-lora/src/sx126x/mod.rs
@@ -55,10 +55,10 @@ where
     BUS: Error + Format + 'static,
 {
     fn get_rx_window_offset_ms(&self) -> i32 {
-        -3
+        -50
     }
     fn get_rx_window_duration_ms(&self) -> u32 {
-        1003
+        1050
     }
 }
 
