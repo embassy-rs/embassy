1806422763be4e9e47201f741a03e5968fc8dedb
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-01 18:16:29 +0200

    stm32/test: add real defmt timestamp

--- tests/stm32/src/example_common.rs
+++ tests/stm32/src/example_common.rs
@@ -1,21 +1,10 @@
 #![macro_use]
 
-use core::sync::atomic::{AtomicUsize, Ordering};
-
 pub use defmt::*;
 #[allow(unused)]
 use embassy_stm32::time::Hertz;
 use embassy_stm32::Config;
 use {defmt_rtt as _, panic_probe as _};
-
-defmt::timestamp! {"{=u64}", {
-        static COUNT: AtomicUsize = AtomicUsize::new(0);
-        // NOTE(no-CAS) `timestamps` runs with interrupts disabled
-        let n = COUNT.load(Ordering::Relaxed);
-        COUNT.store(n + 1, Ordering::Relaxed);
-        n as u64
-    }
-}
 
 pub fn config() -> Config {
     #[allow(unused_mut)]
