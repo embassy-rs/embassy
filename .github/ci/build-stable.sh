421ee4dfbfbbdc007265498d4f529687c16d89af
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-05-29 19:46:46 +0200

    ci: add stable build, add tests.

--- .github/ci/build-stable.sh
+++ .github/ci/build-stable.sh
@@ -4,17 +4,13 @@
 
 set -euo pipefail
 
-echo Hello World!
-
 export RUSTUP_HOME=/ci/cache/rustup
 export CARGO_HOME=/ci/cache/cargo
 export CARGO_TARGET_DIR=/ci/cache/target
-if [ -f /ci/secrets/teleprobe-token.txt ]; then 
-    echo Got teleprobe token!
-    export TELEPROBE_TOKEN=$(cat /ci/secrets/teleprobe-token.txt)
-fi
 
 hashtime restore /ci/cache/filetime.json || true
 hashtime save /ci/cache/filetime.json
 
-./ci.sh
+sed -i 's/channel.*/channel = "stable"/g' rust-toolchain.toml
+
+./ci_stable.sh
