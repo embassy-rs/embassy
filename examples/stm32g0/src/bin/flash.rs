5b4c099afcaa10028912ddc4edfa73c7bce0ffc1
dev-guruprasath <96348958+dev-guruprasath@users.noreply.github.com>
2023-07-31 15:06:15 +0530

    added working example for flash

--- examples/stm32g0/src/bin/flash.rs
+++ examples/stm32g0/src/bin/flash.rs
@@ -4,11 +4,8 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 use embassy_stm32::flash::Flash;
-
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
