ca588f901f5987b8335f380ef8b5dddd7060f75b
René van Dorst <opensource@vdorst.com>
2023-08-21 20:44:42 +0200

    add embassy-net-adin1110 to .github/ci/doc.sh

--- embassy-net-adin1110/src/lib.rs
+++ embassy-net-adin1110/src/lib.rs
@@ -4,6 +4,7 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::missing_panics_doc)]
+#![doc = include_str!("../README.md")]
 
 mod crc32;
 mod crc8;
