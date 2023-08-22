9e3266b74554ea397bdd963ff12a26aa51e77b63
xoviat <xoviat@users.noreply.github.com>
2023-08-22 16:58:19 -0500

    rtc: make fns private

--- embassy-stm32/src/rtc/v2.rs
+++ embassy-stm32/src/rtc/v2.rs
@@ -160,7 +160,7 @@ impl super::Rtc {
     ///
     /// note: this api is exposed for testing purposes until low power is implemented.
     /// it is not intended to be public
-    pub fn start_wakeup_alarm(requested_duration: embassy_time::Duration) -> RtcInstant {
+    pub(crate) fn start_wakeup_alarm(requested_duration: embassy_time::Duration) -> RtcInstant {
         use embassy_time::{Duration, TICK_HZ};
 
         use crate::interrupt::typelevel::Interrupt;
@@ -215,7 +215,7 @@ impl super::Rtc {
     ///
     /// note: this api is exposed for testing purposes until low power is implemented.
     /// it is not intended to be public
-    pub fn stop_wakeup_alarm() -> RtcInstant {
+    pub(crate) fn stop_wakeup_alarm() -> RtcInstant {
         use crate::interrupt::typelevel::Interrupt;
 
         crate::interrupt::typelevel::RTC_WKUP::disable();
