4b695120fce8bb434bf8f20eb964358e37417765
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-09-25 22:34:18 +0200

    tests/stm32: centralize pin configuration.

--- tests/stm32/src/bin/usart.rs
+++ tests/stm32/src/bin/usart.rs
@@ -9,34 +9,7 @@ use defmt::assert_eq;
 use embassy_executor::Spawner;
 use embassy_stm32::dma::NoDma;
 use embassy_stm32::usart::{Config, Error, Uart};
-use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_time::{Duration, Instant};
-
-#[cfg(any(
-    feature = "stm32f103c8",
-    feature = "stm32g491re",
-    feature = "stm32g071rb",
-    feature = "stm32h755zi",
-    feature = "stm32c031c6",
-))]
-bind_interrupts!(struct Irqs {
-    USART1 => usart::InterruptHandler<peripherals::USART1>;
-});
-
-#[cfg(feature = "stm32u585ai")]
-bind_interrupts!(struct Irqs {
-    USART3 => usart::InterruptHandler<peripherals::USART3>;
-});
-
-#[cfg(feature = "stm32f429zi")]
-bind_interrupts!(struct Irqs {
-    USART6 => usart::InterruptHandler<peripherals::USART6>;
-});
-
-#[cfg(any(feature = "stm32wb55rg", feature = "stm32h563zi"))]
-bind_interrupts!(struct Irqs {
-    LPUART1 => usart::InterruptHandler<peripherals::LPUART1>;
-});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -45,28 +18,14 @@ async fn main(_spawner: Spawner) {
 
     // Arduino pins D0 and D1
     // They're connected together with a 1K resistor.
-    #[cfg(feature = "stm32f103c8")]
-    let (mut tx, mut rx, mut usart) = (p.PA9, p.PA10, p.USART1);
-    #[cfg(feature = "stm32g491re")]
-    let (mut tx, mut rx, mut usart) = (p.PC4, p.PC5, p.USART1);
-    #[cfg(feature = "stm32g071rb")]
-    let (mut tx, mut rx, mut usart) = (p.PC4, p.PC5, p.USART1);
-    #[cfg(feature = "stm32f429zi")]
-    let (mut tx, mut rx, mut usart) = (p.PG14, p.PG9, p.USART6);
-    #[cfg(feature = "stm32wb55rg")]
-    let (mut tx, mut rx, mut usart) = (p.PA2, p.PA3, p.LPUART1);
-    #[cfg(feature = "stm32h755zi")]
-    let (mut tx, mut rx, mut usart) = (p.PB6, p.PB7, p.USART1);
-    #[cfg(feature = "stm32u585ai")]
-    let (mut tx, mut rx, mut usart) = (p.PD8, p.PD9, p.USART3);
-    #[cfg(feature = "stm32h563zi")]
-    let (mut tx, mut rx, mut usart) = (p.PB6, p.PB7, p.LPUART1);
-    #[cfg(feature = "stm32c031c6")]
-    let (mut tx, mut rx, mut usart) = (p.PB6, p.PB7, p.USART1);
+    let mut usart = peri!(p, UART);
+    let mut rx = peri!(p, UART_RX);
+    let mut tx = peri!(p, UART_TX);
+    let irq = irqs!(UART);
 
     {
         let config = Config::default();
-        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, Irqs, NoDma, NoDma, config);
+        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, irq, NoDma, NoDma, config);
 
         // We can't send too many bytes, they have to fit in the FIFO.
         // This is because we aren't sending+receiving at the same time.
@@ -82,7 +41,7 @@ async fn main(_spawner: Spawner) {
     // Test error handling with with an overflow error
     {
         let config = Config::default();
-        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, Irqs, NoDma, NoDma, config);
+        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, irq, NoDma, NoDma, config);
 
         // Send enough bytes to fill the RX FIFOs off all USART versions.
         let data = [0xC0, 0xDE, 0x12, 0x23, 0x34];
@@ -116,7 +75,7 @@ async fn main(_spawner: Spawner) {
 
         let mut config = Config::default();
         config.baudrate = baudrate;
-        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, Irqs, NoDma, NoDma, config);
+        let mut usart = Uart::new(&mut usart, &mut rx, &mut tx, irq, NoDma, NoDma, config);
 
         let n = (baudrate as usize / 100).max(64);
 
--- tests/stm32/src/bin/usart_rx_ringbuffered.rs
+++ tests/stm32/src/bin/usart_rx_ringbuffered.rs
@@ -10,86 +10,9 @@ use common::*;
 use defmt::{assert_eq, panic};
 use embassy_executor::Spawner;
 use embassy_stm32::usart::{Config, DataBits, Parity, RingBufferedUartRx, StopBits, Uart, UartTx};
-use embassy_stm32::{bind_interrupts, peripherals, usart};
 use embassy_time::{Duration, Timer};
 use rand_chacha::ChaCha8Rng;
 use rand_core::{RngCore, SeedableRng};
-
-#[cfg(any(
-    feature = "stm32f103c8",
-    feature = "stm32g491re",
-    feature = "stm32g071rb",
-    feature = "stm32h755zi",
-    feature = "stm32c031c6",
-))]
-bind_interrupts!(struct Irqs {
-    USART1 => usart::InterruptHandler<peripherals::USART1>;
-});
-
-#[cfg(feature = "stm32u585ai")]
-bind_interrupts!(struct Irqs {
-    USART3 => usart::InterruptHandler<peripherals::USART3>;
-});
-
-#[cfg(feature = "stm32f429zi")]
-bind_interrupts!(struct Irqs {
-    USART1 => usart::InterruptHandler<peripherals::USART1>;
-    USART6 => usart::InterruptHandler<peripherals::USART6>;
-});
-
-#[cfg(any(feature = "stm32wb55rg", feature = "stm32h563zi"))]
-bind_interrupts!(struct Irqs {
-    LPUART1 => usart::InterruptHandler<peripherals::LPUART1>;
-});
-
-#[cfg(feature = "stm32f103c8")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH4;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH5;
-}
-#[cfg(feature = "stm32g491re")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH1;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH2;
-}
-#[cfg(feature = "stm32g071rb")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH1;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH2;
-}
-#[cfg(feature = "stm32f429zi")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART6;
-    pub type TxDma = embassy_stm32::peripherals::DMA2_CH6;
-    pub type RxDma = embassy_stm32::peripherals::DMA2_CH1;
-}
-#[cfg(feature = "stm32wb55rg")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::LPUART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH1;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH2;
-}
-#[cfg(feature = "stm32h755zi")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH0;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH1;
-}
-#[cfg(feature = "stm32u585ai")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART3;
-    pub type TxDma = embassy_stm32::peripherals::GPDMA1_CH0;
-    pub type RxDma = embassy_stm32::peripherals::GPDMA1_CH1;
-}
-#[cfg(feature = "stm32c031c6")]
-mod board {
-    pub type Uart = embassy_stm32::peripherals::USART1;
-    pub type TxDma = embassy_stm32::peripherals::DMA1_CH1;
-    pub type RxDma = embassy_stm32::peripherals::DMA1_CH2;
-}
 
 const DMA_BUF_SIZE: usize = 256;
 
@@ -100,22 +23,12 @@ async fn main(spawner: Spawner) {
 
     // Arduino pins D0 and D1
     // They're connected together with a 1K resistor.
-    #[cfg(feature = "stm32f103c8")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PA9, p.PA10, p.USART1, p.DMA1_CH4, p.DMA1_CH5);
-    #[cfg(feature = "stm32g491re")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PC4, p.PC5, p.USART1, p.DMA1_CH1, p.DMA1_CH2);
-    #[cfg(feature = "stm32g071rb")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PC4, p.PC5, p.USART1, p.DMA1_CH1, p.DMA1_CH2);
-    #[cfg(feature = "stm32f429zi")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PG14, p.PG9, p.USART6, p.DMA2_CH6, p.DMA2_CH1);
-    #[cfg(feature = "stm32wb55rg")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PA2, p.PA3, p.LPUART1, p.DMA1_CH1, p.DMA1_CH2);
-    #[cfg(feature = "stm32h755zi")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PB6, p.PB7, p.USART1, p.DMA1_CH0, p.DMA1_CH1);
-    #[cfg(feature = "stm32u585ai")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PD8, p.PD9, p.USART3, p.GPDMA1_CH0, p.GPDMA1_CH1);
-    #[cfg(feature = "stm32c031c6")]
-    let (tx, rx, usart, tx_dma, rx_dma) = (p.PB6, p.PB7, p.USART1, p.DMA1_CH1, p.DMA1_CH2);
+    let usart = peri!(p, UART);
+    let rx = peri!(p, UART_RX);
+    let tx = peri!(p, UART_TX);
+    let rx_dma = peri!(p, UART_RX_DMA);
+    let tx_dma = peri!(p, UART_TX_DMA);
+    let irq = irqs!(UART);
 
     // To run this test, use the saturating_serial test utility to saturate the serial port
 
@@ -127,7 +40,7 @@ async fn main(spawner: Spawner) {
     config.stop_bits = StopBits::STOP1;
     config.parity = Parity::ParityNone;
 
-    let usart = Uart::new(usart, rx, tx, Irqs, tx_dma, rx_dma, config);
+    let usart = Uart::new(usart, rx, tx, irq, tx_dma, rx_dma, config);
     let (tx, rx) = usart.split();
     static mut DMA_BUF: [u8; DMA_BUF_SIZE] = [0; DMA_BUF_SIZE];
     let dma_buf = unsafe { DMA_BUF.as_mut() };
@@ -139,7 +52,7 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn transmit_task(mut tx: UartTx<'static, board::Uart, board::TxDma>) {
+async fn transmit_task(mut tx: UartTx<'static, peris::UART, peris::UART_TX_DMA>) {
     // workaround https://github.com/embassy-rs/embassy/issues/1426
     Timer::after(Duration::from_millis(100) as _).await;
 
@@ -162,7 +75,7 @@ async fn transmit_task(mut tx: UartTx<'static, board::Uart, board::TxDma>) {
 }
 
 #[embassy_executor::task]
-async fn receive_task(mut rx: RingBufferedUartRx<'static, board::Uart, board::RxDma>) {
+async fn receive_task(mut rx: RingBufferedUartRx<'static, peris::UART, peris::UART_RX_DMA>) {
     info!("Ready to receive...");
 
     let mut rng = ChaCha8Rng::seed_from_u64(1337);
