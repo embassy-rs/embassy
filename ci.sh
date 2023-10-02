00824af82ba8bfa5cd8226ee057719595d8d10af
xoviat <xoviat@users.noreply.github.com>
2023-10-02 18:55:31 -0500

    ci: remove wpan tests

--- ci.sh
+++ ci.sh
@@ -197,6 +197,9 @@ cargo batch  \
     $BUILD_EXTRA
 
 
+rm out/tests/stm32wb55rg/wpan_mac
+rm out/tests/stm32wb55rg/wpan_ble
+
 if [[ -z "${TELEPROBE_TOKEN-}" ]]; then
     echo No teleprobe token found, skipping running HIL tests
     exit
