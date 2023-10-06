9090a78df4c3838ee8fdfd6641f5fea7d8e9906e
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-10-07 01:33:43 +0200

    ci: set target dir if not specified.

--- ci.sh
+++ ci.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 export RUSTFLAGS=-Dwarnings
 export DEFMT_LOG=trace,embassy_hal_internal=debug,embassy_net_esp_hosted=debug,cyw43=info,cyw43_pio=info,smoltcp=info
+if [[ -z "${CARGO_TARGET_DIR}" ]]; then
+    export CARGO_TARGET_DIR=target_ci
+fi
 
 TARGET=$(rustc -vV | sed -n 's|host: ||p')
 
