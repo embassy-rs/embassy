063e6f96daecf8f68a538c0f1904619d84333926
Andres Oliva <andres.oliva@teufel.de>
2023-10-11 23:56:21 +0200

    Remove outdated comment

--- embassy-stm32/src/i2c/mod.rs
+++ embassy-stm32/src/i2c/mod.rs
@@ -2,8 +2,6 @@
 
 use crate::interrupt;
 
-// I2C V2 is gated on the `time` feature because timing facilities are necessary
-// to provide timeout functionality in order to prevent some APIs from stalling indefinitely
 #[cfg_attr(i2c_v1, path = "v1.rs")]
 #[cfg_attr(i2c_v2, path = "v2.rs")]
 mod _version;
