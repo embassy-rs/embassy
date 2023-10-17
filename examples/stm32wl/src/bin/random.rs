b47864046381cccdef186321fc0d46a1dd2a45be
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-10-17 15:51:46 +0200

    fix clocks in stm32wl rng example.

--- examples/stm32wl/src/bin/random.rs
+++ examples/stm32wl/src/bin/random.rs
@@ -4,6 +4,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::rcc::{ClockSrc, MSIRange};
 use embassy_stm32::rng::{self, Rng};
 use embassy_stm32::{bind_interrupts, pac, peripherals};
 use {defmt_rtt as _, panic_probe as _};
@@ -15,12 +16,10 @@ bind_interrupts!(struct Irqs{
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let mut config = embassy_stm32::Config::default();
-    config.rcc.mux = embassy_stm32::rcc::ClockSrc::HSE;
-
+    config.rcc.mux = ClockSrc::MSI(MSIRange::RANGE32M);
     let p = embassy_stm32::init(config);
-    pac::RCC.ccipr().modify(|w| {
-        w.set_rngsel(0b01);
-    });
+
+    pac::RCC.ccipr().modify(|w| w.set_rngsel(0b11)); // msi
 
     info!("Hello World!");
 
