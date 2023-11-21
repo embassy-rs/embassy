ba9cc102e295634035a3f9bac1b4656e735c5fc6
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2023-11-21 16:53:28 +0100

    ci: cache lockfiles
    - Removes the slow "updating crates.io index, updating git" at start of the job.
    - Avoids CI being randomly slower if a widely-used dep (like serde) gets bumped causing rebuilding everything.

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -18,7 +18,17 @@ fi
 # used when pointing stm32-metapac to a CI-built one.
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+# Restore lockfiles
+if [ -f /ci/cache/lockfiles.tar ]; then
+    echo Restoring lockfiles...
+    tar xf /ci/cache/lockfiles.tar
+fi
+
 hashtime restore /ci/cache/filetime.json || true
 hashtime save /ci/cache/filetime.json
 
 ./ci.sh
+
+# Save lockfiles
+echo Saving lockfiles...
+find . -type f -name Cargo.lock -exec tar -cf /ci/cache/lockfiles.tar '{}' \+
