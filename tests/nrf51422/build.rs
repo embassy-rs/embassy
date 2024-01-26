ee90ee185c150085ee59c873e016b6f88f646b56
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2024-01-26 08:58:23 +0100

    fix: link nrf51 tests from flash for now

--- tests/nrf51422/build.rs
+++ tests/nrf51422/build.rs
@@ -4,12 +4,12 @@ use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-    fs::write(out.join("link_ram.x"), include_bytes!("../link_ram_cortex_m.x")).unwrap();
+    fs::write(out.join("memory.x"), include_bytes!("memory.x")).unwrap();
     println!("cargo:rustc-link-search={}", out.display());
-    println!("cargo:rerun-if-changed=link_ram.x");
+    println!("cargo:rerun-if-changed=memory.x");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
-    println!("cargo:rustc-link-arg-bins=-Tlink_ram.x");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
     println!("cargo:rustc-link-arg-bins=-Tteleprobe.x");
 
