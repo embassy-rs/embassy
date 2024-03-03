b0f05e768225ae321cb08a469fa4a384cb8523ab
Corey Schuhen <cschuhen@gmail.com>
2024-03-03 20:45:51 +1000

    Remove unused.

--- embassy-stm32/src/can/bx/mod.rs
+++ embassy-stm32/src/can/bx/mod.rs
@@ -27,7 +27,6 @@
 pub mod filter;
 mod frame;
 mod id;
-mod interrupt;
 
 #[allow(clippy::all)] // generated code
 mod pac;
@@ -43,7 +42,6 @@ pub use id::{ExtendedId, Id, StandardId};
 use self::pac::generic::*;
 use crate::can::bx::filter::MasterFilters;
 pub use crate::can::bx::frame::{Data, Frame, FramePriority};
-pub use crate::can::bx::interrupt::{Interrupt, Interrupts};
 pub use crate::can::bx::pac::can::RegisterBlock; // To make the PAC extraction build
 
 /// A bxCAN peripheral instance.
@@ -549,76 +547,6 @@ where
                 break;
             }
         }
-    }
-
-    /// Starts listening for a CAN interrupt.
-    pub fn enable_interrupt(&mut self, interrupt: Interrupt) {
-        self.enable_interrupts(Interrupts::from_bits_truncate(interrupt as u32))
-    }
-
-    /// Starts listening for a set of CAN interrupts.
-    pub fn enable_interrupts(&mut self, interrupts: Interrupts) {
-        self.registers()
-            .ier
-            .modify(|r, w| unsafe { w.bits(r.bits() | interrupts.bits()) })
-    }
-
-    /// Stops listening for a CAN interrupt.
-    pub fn disable_interrupt(&mut self, interrupt: Interrupt) {
-        self.disable_interrupts(Interrupts::from_bits_truncate(interrupt as u32))
-    }
-
-    /// Stops listening for a set of CAN interrupts.
-    pub fn disable_interrupts(&mut self, interrupts: Interrupts) {
-        self.registers()
-            .ier
-            .modify(|r, w| unsafe { w.bits(r.bits() & !interrupts.bits()) })
-    }
-
-    /// Clears the pending flag of [`Interrupt::Sleep`].
-    pub fn clear_sleep_interrupt(&self) {
-        let can = self.registers();
-        // Read-only register with write-1-to-clear, so `&self` is sufficient.
-        can.msr.write(|w| w.slaki().set_bit());
-    }
-
-    /// Clears the pending flag of [`Interrupt::Wakeup`].
-    pub fn clear_wakeup_interrupt(&self) {
-        let can = self.registers();
-        // Read-only register with write-1-to-clear, so `&self` is sufficient.
-        can.msr.write(|w| w.wkui().set_bit());
-    }
-
-    /// Clears the "Request Completed" (RQCP) flag of a transmit mailbox.
-    ///
-    /// Returns the [`Mailbox`] whose flag was cleared. If no mailbox has the flag set, returns
-    /// `None`.
-    ///
-    /// Once this function returns `None`, a pending [`Interrupt::TransmitMailboxEmpty`] is
-    /// considered acknowledged.
-    pub fn clear_request_completed_flag(&mut self) -> Option<Mailbox> {
-        let can = self.registers();
-        let tsr = can.tsr.read();
-        if tsr.rqcp0().bit_is_set() {
-            can.tsr.modify(|_, w| w.rqcp0().set_bit());
-            Some(Mailbox::Mailbox0)
-        } else if tsr.rqcp1().bit_is_set() {
-            can.tsr.modify(|_, w| w.rqcp1().set_bit());
-            Some(Mailbox::Mailbox1)
-        } else if tsr.rqcp2().bit_is_set() {
-            can.tsr.modify(|_, w| w.rqcp2().set_bit());
-            Some(Mailbox::Mailbox2)
-        } else {
-            None
-        }
-    }
-
-    /// Clears a pending TX interrupt ([`Interrupt::TransmitMailboxEmpty`]).
-    ///
-    /// This does not return the mailboxes that have finished tranmission. If you need that
-    /// information, call [`Can::clear_request_completed_flag`] instead.
-    pub fn clear_tx_interrupt(&mut self) {
-        while self.clear_request_completed_flag().is_some() {}
     }
 
     /// Puts a CAN frame in a free transmit mailbox for transmission on the bus.
