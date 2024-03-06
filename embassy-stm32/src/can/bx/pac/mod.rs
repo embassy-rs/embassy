98e7a0a423de98c32863e2949b381efa7b5cdead
Corey Schuhen <cschuhen@gmail.com>
2024-03-07 08:09:49 +1000

    Remove old PAC from bscan crate.

--- embassy-stm32/src/can/bx/pac/mod.rs
+++
@@ -1,9 +0,0 @@
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(unused)]
-use core::marker::PhantomData;
-use core::ops::Deref;
-
-#[doc = "Controller area network"]
-pub mod can;
-pub mod generic;
