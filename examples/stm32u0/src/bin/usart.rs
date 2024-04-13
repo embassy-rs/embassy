5f23e3905226551a001da9bf4051c6efb552f636
Dion Dokter <diondokter@gmail.com>
2024-04-13 18:40:46 +0200

    Add some examples.
    - usart works
    - dac works
    - rng gets stuck on while loop
    - usb_serial works, but cannot test due to lack of user usb port
    - adc needs work and does not work yet

---
+++ examples/stm32u0/src/bin/usart.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_stm32::dma::NoDma;
+use embassy_stm32::usart::{Config, Uart};
+use embassy_stm32::{bind_interrupts, peripherals, usart};
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    USART2_LPUART2 => usart::InterruptHandler<peripherals::USART2>;
+});
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    info!("Hello World!");
+
+    let p = embassy_stm32::init(Default::default());
+
+    let config = Config::default();
+    let mut usart = Uart::new(p.USART2, p.PA3, p.PA2, Irqs, NoDma, NoDma, config).unwrap();
+
+    unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
+    info!("wrote Hello, starting echo");
+
+    let mut buf = [0u8; 1];
+    loop {
+        unwrap!(usart.blocking_read(&mut buf));
+        unwrap!(usart.blocking_write(&buf));
+    }
+}
