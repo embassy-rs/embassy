e147f29b5ccbe94fb0703a4cb37ce6a403c321f3
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2024-04-14 22:21:19 +0200

    ci: test embassy-futures.

--- .github/ci/test.sh
+++ .github/ci/test.sh
@@ -8,6 +8,7 @@ export RUSTUP_HOME=/ci/cache/rustup
 export CARGO_HOME=/ci/cache/cargo
 export CARGO_TARGET_DIR=/ci/cache/target
 
+cargo test --manifest-path ./embassy-futures/Cargo.toml
 cargo test --manifest-path ./embassy-sync/Cargo.toml
 cargo test --manifest-path ./embassy-embedded-hal/Cargo.toml
 cargo test --manifest-path ./embassy-hal-internal/Cargo.toml
