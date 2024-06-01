16c82c4196de204b268e16ab10fa151142c69d73
Jan Špaček <patek.mail@gmail.com>
2024-06-01 11:02:26 +0200

    stm32/timer: Remove generics and introduce type-safe timer taxonomy

--- embassy-stm32/src/dma/util.rs
+++ embassy-stm32/src/dma/util.rs
@@ -3,15 +3,18 @@ use embassy_hal_internal::PeripheralRef;
 use super::word::Word;
 use super::{AnyChannel, Request, Transfer, TransferOptions};
 
-/// Convenience wrapper, contains a channel and a request number.
+/// Convenience wrapper, contains a DMA channel and a DMA request number.
 ///
 /// Commonly used in peripheral drivers that own DMA channels.
-pub(crate) struct ChannelAndRequest<'d> {
+pub struct ChannelAndRequest<'d> {
+    /// DMA channel.
     pub channel: PeripheralRef<'d, AnyChannel>,
+    /// DMA request.
     pub request: Request,
 }
 
 impl<'d> ChannelAndRequest<'d> {
+    /// See [`Transfer::new_read()`].
     pub unsafe fn read<'a, W: Word>(
         &'a mut self,
         peri_addr: *mut W,
@@ -21,6 +24,7 @@ impl<'d> ChannelAndRequest<'d> {
         Transfer::new_read(&mut self.channel, self.request, peri_addr, buf, options)
     }
 
+    /// See [`Transfer::new_read_raw()`].
     pub unsafe fn read_raw<'a, W: Word>(
         &'a mut self,
         peri_addr: *mut W,
@@ -30,6 +34,7 @@ impl<'d> ChannelAndRequest<'d> {
         Transfer::new_read_raw(&mut self.channel, self.request, peri_addr, buf, options)
     }
 
+    /// See [`Transfer::new_write()`].
     pub unsafe fn write<'a, W: Word>(
         &'a mut self,
         buf: &'a [W],
@@ -39,6 +44,7 @@ impl<'d> ChannelAndRequest<'d> {
         Transfer::new_write(&mut self.channel, self.request, buf, peri_addr, options)
     }
 
+    /// See [`Transfer::new_write_raw()`].
     pub unsafe fn write_raw<'a, W: Word>(
         &'a mut self,
         buf: *const [W],
@@ -48,7 +54,7 @@ impl<'d> ChannelAndRequest<'d> {
         Transfer::new_write_raw(&mut self.channel, self.request, buf, peri_addr, options)
     }
 
-    #[allow(dead_code)]
+    /// See [`Transfer::new_write_repeated()`].
     pub unsafe fn write_repeated<'a, W: Word>(
         &'a mut self,
         repeated: &'a W,
