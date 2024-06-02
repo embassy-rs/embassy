b982bf2e27f6e893d17953b7a3462b7c9c01f39d
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2024-06-02 17:07:28 -0400

    improve examples

--- examples/stm32f4/src/bin/input_capture.rs
+++ examples/stm32f4/src/bin/input_capture.rs
@@ -3,7 +3,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Pull, Speed};
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Pull, Speed};
 use embassy_stm32::time::khz;
 use embassy_stm32::timer::input_capture::{CapturePin, InputCapture};
 use embassy_stm32::timer::{self, Channel};
@@ -14,7 +14,7 @@ use {defmt_rtt as _, panic_probe as _};
 /// Connect PB2 and PB10 with a 1k Ohm resistor
 
 #[embassy_executor::task]
-async fn blinky(led: peripherals::PB2) {
+async fn blinky(led: AnyPin) {
     let mut led = Output::new(led, Level::High, Speed::Low);
 
     loop {
@@ -37,7 +37,7 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    unwrap!(spawner.spawn(blinky(p.PB2)));
+    unwrap!(spawner.spawn(blinky(p.PB2.degrade())));
 
     let ch3 = CapturePin::new_ch3(p.PB10, Pull::None);
     let mut ic = InputCapture::new(p.TIM2, None, None, Some(ch3), None, Irqs, khz(1000), Default::default());
--- examples/stm32f4/src/bin/pwm_input.rs
+++ examples/stm32f4/src/bin/pwm_input.rs
@@ -3,7 +3,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Pull, Speed};
+use embassy_stm32::gpio::{AnyPin, Level, Output, Pin, Pull, Speed};
 use embassy_stm32::time::khz;
 use embassy_stm32::timer::pwm_input::PwmInput;
 use embassy_stm32::{bind_interrupts, peripherals, timer};
@@ -13,7 +13,7 @@ use {defmt_rtt as _, panic_probe as _};
 /// Connect PB2 and PA6 with a 1k Ohm resistor
 
 #[embassy_executor::task]
-async fn blinky(led: peripherals::PB2) {
+async fn blinky(led: AnyPin) {
     let mut led = Output::new(led, Level::High, Speed::Low);
 
     loop {
@@ -36,12 +36,13 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    unwrap!(spawner.spawn(blinky(p.PB2)));
+    unwrap!(spawner.spawn(blinky(p.PB2.degrade())));
 
     let mut pwm_input = PwmInput::new(p.TIM3, p.PA6, Pull::None, Irqs, khz(10));
     pwm_input.enable();
 
     loop {
+        info!("wait for rising edge");
         let width = pwm_input.wait_for_falling_edge().await;
         info!("pulse width: {}", width);
     }
