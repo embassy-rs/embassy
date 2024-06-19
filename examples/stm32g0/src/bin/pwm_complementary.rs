0579d248efc1ee78f039fbfabcf2e0272e32ee53
Chen Yuheng <1016867898@qq.com>
2024-06-19 10:57:18 +0800

    Add PWM examples for stm32g0

---
+++ examples/stm32g0/src/bin/pwm_complementary.rs
@@ -0,0 +1,58 @@
+//! PWM complementary example
+//!
+//! This example uses two complementary pwm outputs from TIM1 with different duty cycles
+//!   ___           ___
+//!      |_________|   |_________|    PA8
+//!       _________     _________
+//!   ___|         |___|         |    PA7
+//!   _________     _________
+//!            |___|         |___|    PB3
+//!             ___           ___
+//!   _________|   |_________|   |    PB0
+
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::OutputType;
+use embassy_stm32::time::khz;
+use embassy_stm32::timer::complementary_pwm::{ComplementaryPwm, ComplementaryPwmPin};
+use embassy_stm32::timer::simple_pwm::PwmPin;
+use embassy_stm32::timer::Channel;
+use embassy_stm32::Config as PeripheralConfig;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let ch1 = PwmPin::new_ch1(p.PA8, OutputType::PushPull);
+    let ch1n = ComplementaryPwmPin::new_ch1(p.PA7, OutputType::PushPull);
+    let ch2 = PwmPin::new_ch2(p.PB3, OutputType::PushPull);
+    let ch2n = ComplementaryPwmPin::new_ch2(p.PB0, OutputType::PushPull);
+
+    let mut pwm = ComplementaryPwm::new(
+        p.TIM1,
+        Some(ch1),
+        Some(ch1n),
+        Some(ch2),
+        Some(ch2n),
+        None,
+        None,
+        None,
+        None,
+        khz(100),
+        Default::default(),
+    );
+
+    let max = pwm.get_max_duty();
+    info!("Max duty: {}", max);
+
+    pwm.set_duty(Channel::Ch1, max / 4);
+    pwm.enable(Channel::Ch1);
+    pwm.set_duty(Channel::Ch2, max * 3 / 4);
+    pwm.enable(Channel::Ch2);
+
+    loop {}
+}
