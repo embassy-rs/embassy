5a16977a4ab72aa13bcf6c1b5cd517a3162deaba
seth <seth@transastra.com>
2024-06-24 00:32:25 -0700

    move dma adc to a new file & struct.

---
+++ embassy-stm32/src/adc/ringbuffered_v2.rs
@@ -0,0 +1,209 @@
+use core::future::poll_fn;
+use core::marker::PhantomData;
+use core::mem;
+use core::sync::atomic::{compiler_fence, Ordering};
+use core::task::Poll;
+
+use embassy_hal_internal::into_ref;
+
+use super::{Adc, RxDma};
+use crate::adc::Instance;
+use crate::dma::ringbuffer::OverrunError;
+use crate::dma::ReadableRingBuffer;
+use crate::pac::adc::vals;
+
+fn clear_interrupt_flags(r: crate::pac::adc::Adc) {
+    r.sr().modify(|regs| {
+        regs.set_eoc(false);
+        regs.set_ovr(false);
+    });
+}
+
+pub struct RingBufferedAdc<'d, T: Instance> {
+    _phantom: PhantomData<T>,
+    // rx_dma: PeripheralRef<'d, S>,
+    ring_buf: ReadableRingBuffer<'d, u16>,
+}
+
+impl<'d, T: Instance> Adc<'d, T> {
+    pub fn into_ring_buffered(self, rx_dma: impl RxDma<T>, dma_buf: &'d mut [u16]) -> RingBufferedAdc<'d, T> {
+        assert!(!dma_buf.is_empty() && dma_buf.len() <= 0xFFFF);
+        into_ref!(rx_dma);
+        let opts = Default::default();
+
+        let rx_src = T::regs().dr().as_ptr() as *mut u16;
+        // Safety: we forget the struct before this function returns.
+        let request = rx_dma.request();
+
+        let ring_buf = unsafe { ReadableRingBuffer::new(rx_dma, request, rx_src, dma_buf, opts) };
+
+        // Don't disable the clock
+        mem::forget(self);
+
+        RingBufferedAdc {
+            _phantom: PhantomData,
+            ring_buf,
+        }
+    }
+}
+
+impl<'d, T: Instance> RingBufferedAdc<'d, T> {
+    fn is_on() -> bool {
+        T::regs().cr2().read().adon()
+    }
+
+    pub fn start(&mut self) -> Result<(), OverrunError> {
+        self.ring_buf.clear();
+
+        self.setup_adc();
+
+        Ok(())
+    }
+
+    fn stop(&mut self, err: OverrunError) -> Result<usize, OverrunError> {
+        self.teardown_adc();
+        Err(err)
+    }
+
+    pub fn teardown_adc(&mut self) {
+        self.ring_buf.request_stop();
+
+        let r = T::regs();
+
+        // Stop ADC
+        r.cr2().modify(|reg| {
+            // Stop ADC
+            reg.set_swstart(false);
+            // Stop DMA
+            reg.set_dma(false);
+        });
+
+        r.cr1().modify(|w| {
+            // Disable interrupt for end of conversion
+            w.set_eocie(false);
+            // Disable interrupt for overrun
+            w.set_ovrie(false);
+        });
+
+        clear_interrupt_flags(r);
+
+        compiler_fence(Ordering::SeqCst);
+    }
+
+    fn setup_adc(&mut self) {
+        compiler_fence(Ordering::SeqCst);
+
+        self.ring_buf.start();
+
+        let r = T::regs();
+
+        //Enable ADC
+        let was_on = Self::is_on();
+        if !was_on {
+            r.cr2().modify(|reg| {
+                reg.set_adon(true);
+                reg.set_swstart(false);
+            });
+        }
+
+        // Clear all interrupts
+        r.sr().modify(|regs| {
+            regs.set_eoc(false);
+            regs.set_ovr(false);
+        });
+
+        r.cr1().modify(|w| {
+            // Enable interrupt for end of conversion
+            w.set_eocie(true);
+            // Enable interrupt for overrun
+            w.set_ovrie(true);
+            // Scanning converisons of multiple channels
+            w.set_scan(true);
+            // Continuous conversion mode
+            w.set_discen(false);
+        });
+
+        r.cr2().modify(|w| {
+            // Enable DMA mode
+            w.set_dma(true);
+            // Enable continuous conversions
+            w.set_cont(vals::Cont::CONTINUOUS);
+            // DMA requests are issues as long as DMA=1 and data are converted.
+            w.set_dds(vals::Dds::CONTINUOUS);
+            // EOC flag is set at the end of each conversion.
+            w.set_eocs(vals::Eocs::EACHSEQUENCE);
+        });
+
+        //Being ADC conversions
+        T::regs().cr2().modify(|reg| {
+            reg.set_adon(true);
+            reg.set_swstart(true);
+        });
+    }
+
+    /// Read bytes that are readily available in the ring buffer.
+    /// If no bytes are currently available in the buffer the call waits until the some
+    /// bytes are available (at least one byte and at most half the buffer size)
+    ///
+    /// Background receive is started if `start()` has not been previously called.
+    ///
+    /// Receive in the background is terminated if an error is returned.
+    /// It must then manually be started again by calling `start()` or by re-calling `read()`.
+    pub async fn read(&mut self, buf: &mut [u16]) -> Result<usize, OverrunError> {
+        let r = T::regs();
+
+        // Start background receive if it was not already started
+        if !r.cr2().read().dma() {
+            self.start()?;
+        }
+
+        // Clear overrun flag if set.
+        if r.sr().read().ovr() {
+            r.sr().modify(|regs| {
+                regs.set_ovr(false);
+                regs.set_eoc(false);
+            });
+            trace!("FLAG OVR");
+            return self.stop(OverrunError);
+        }
+
+        loop {
+            match self.ring_buf.read(buf) {
+                Ok((0, _)) => {}
+                Ok((len, _)) => {
+                    return Ok(len);
+                }
+                Err(_) => {
+                    trace!("OVERRUN on READ");
+                    return self.stop(OverrunError);
+                }
+            }
+        }
+    }
+
+    //     async fn wait_for_data(&mut self) {
+    //         compiler_fence(Ordering::SeqCst);
+
+    //         let mut dma_init = false;
+    //         // Future which completes when there is dma is half full or full
+    //         let dma = poll_fn(|cx| {
+    //             self.ring_buf.set_waker(cx.waker());
+
+    //             let status = match dma_init {
+    //                 false => Poll::Pending,
+    //                 true => Poll::Ready(()),
+    //             };
+
+    //             dma_init = true;
+    //             status
+    //         });
+    //         dma.await;
+    //     }
+}
+
+impl<T: Instance> Drop for RingBufferedAdc<'_, T> {
+    fn drop(&mut self) {
+        self.teardown_adc();
+        T::disable();
+    }
+}
--- examples/stm32f4/src/bin/adc_dma.rs
+++ examples/stm32f4/src/bin/adc_dma.rs
@@ -3,71 +3,58 @@
 #![feature(type_alias_impl_trait)]
 #![feature(impl_trait_in_assoc_type)]
 use cortex_m::singleton;
+use cortex_m_rt::entry;
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures::block_on;
+use embassy_stm32::adc::ringbuffered_v2::RingBufferedAdc;
 use embassy_stm32::adc::{Adc, SampleTime, Sequence};
 use embassy_time::{Instant, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    const ADC_BUF_SIZE: usize = 512;
+async fn main(spawner: Spawner) -> ! {
+    const ADC_BUF_SIZE: usize = 1024;
     let mut p = embassy_stm32::init(Default::default());
 
     let adc_data: &mut [u16; ADC_BUF_SIZE] = singleton!(ADCDAT : [u16; ADC_BUF_SIZE] = [0u16; ADC_BUF_SIZE]).unwrap();
-    let adc2_data: &mut [u16; ADC_BUF_SIZE] = singleton!(ADCDAT : [u16; ADC_BUF_SIZE] = [0u16; ADC_BUF_SIZE]).unwrap();
-
     let mut adc = Adc::new(p.ADC1);
-    let mut adc2 = Adc::new(p.ADC2);
 
     adc.set_sample_sequence(Sequence::One, &mut p.PA0, SampleTime::CYCLES112)
         .await;
-
     adc.set_sample_sequence(Sequence::Two, &mut p.PA1, SampleTime::CYCLES112)
         .await;
 
-    adc2.set_sample_sequence(Sequence::One, &mut p.PA2, SampleTime::CYCLES112)
+    adc.set_sample_sequence(Sequence::Three, &mut p.PA2, SampleTime::CYCLES112)
+        .await;
+    //     .await;
+    adc.set_sample_sequence(Sequence::Four, &mut p.PA3, SampleTime::CYCLES112)
         .await;
 
-    adc2.set_sample_sequence(Sequence::Two, &mut p.PA3, SampleTime::CYCLES112)
-        .await;
-
-    let mut adc_dma = adc.start_read_continuous(p.DMA2_CH0, adc_data);
-    let mut adc_dma2 = adc2.start_read_continuous(p.DMA2_CH2, adc2_data);
+    let mut adc: RingBufferedAdc<embassy_stm32::peripherals::ADC1> = adc.into_ring_buffered(p.DMA2_CH0, adc_data);
 
     let mut tic = Instant::now();
-    let mut buffer1 = [0u16; 256];
-    let mut buffer2 = [0u16; 256];
+    let mut buffer1 = [0u16; 32];
+    let _ = adc.start();
     loop {
-        
-        
-        match adc.get_dma_buf(&mut adc_dma, &mut buffer1).await {
-            Ok(_data) => {} //info!("adc1 sa: {}", data),
+        match adc.read(&mut buffer1).await {
+            Ok(_data) => {
+                let toc = Instant::now();
+                info!(
+                    "\n adc1: {} dt = {}, n = {}",
+                    buffer1[0..16],
+                    (toc - tic).as_micros(),
+                    _data
+                );
+                tic = toc;
+            }
             Err(e) => {
                 warn!("Error: {:?}", e);
-                continue;
+                buffer1 = [0u16; 32];
+                let _ = adc.start();
             }
         }
-
-        match adc2.get_dma_buf(&mut adc_dma2, &mut buffer2).await {
-            Ok(_data2) => {} //info!("adc2 sa: {}", data2),
-            Err(e) => {
-                warn!("Error: {:?}", e);
-                continue;
-            }
-        }
-
-        let toc = Instant::now();
-        info!(
-            "\n adc1: {}, adc2: {}, dt = {}",
-            buffer1[0..4],
-            buffer2[0..4],
-            (toc - tic).as_micros()
-        );
-        // info!("{}", (toc - tic).as_micros());
-        tic = toc;
 
         Timer::after_millis(1).await;
-
     }
 }
