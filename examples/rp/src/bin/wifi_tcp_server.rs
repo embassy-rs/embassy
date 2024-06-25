14569002816f23d20f7a651ebcfab0a51333aff8
9names <60134748+9names@users.noreply.github.com>
2024-06-25 20:47:27 +1000

    Replace joke seed with best RNG available

--- examples/rp/src/bin/wifi_tcp_server.rs
+++ examples/rp/src/bin/wifi_tcp_server.rs
@@ -43,6 +43,7 @@ async fn main(spawner: Spawner) {
     info!("Hello World!");
 
     let p = embassy_rp::init(Default::default());
+    let mut rng = RoscRng;
 
     let fw = include_bytes!("../../../../cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../../../../cyw43-firmware/43439A0_clm.bin");
@@ -77,7 +78,7 @@ async fn main(spawner: Spawner) {
     //});
 
     // Generate random seed
-    let seed = 0x0123_4567_89ab_cdef; // chosen by fair dice roll. guarenteed to be random.
+    let seed = rng.next_u64();
 
     // Init network stack
     static STACK: StaticCell<Stack<cyw43::NetDriver<'static>>> = StaticCell::new();
