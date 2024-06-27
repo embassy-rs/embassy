8a6b71b0bba450687c48b36c60273f825c3f6d4b
nerwalt <tlawren@gmail.com>
2024-06-27 14:07:26 -0600

    Adding initial support for nrf9151 s and ns Adding s and ns examples, including a reference TFM

---
+++ examples/nrf9151/ns/src/bin/blinky.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+use defmt;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    let mut led = Output::new(p.P0_00, Level::Low, OutputDrive::Standard);
+
+    loop {
+        led.set_high();
+        defmt::info!("high");
+        Timer::after_millis(500).await;
+        led.set_low();
+        defmt::info!("low");
+        Timer::after_millis(1000).await;
+    }
+}
