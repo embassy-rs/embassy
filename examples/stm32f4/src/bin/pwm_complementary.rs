1b54ca0866752eadbc92c708a6eab797ce762834
Jan Špaček <patek.mail@gmail.com>
2024-06-29 16:19:23 +0200

    stm32/timer: Update examples for new timer API

--- examples/stm32f4/src/bin/pwm_complementary.rs
+++ examples/stm32f4/src/bin/pwm_complementary.rs
@@ -5,9 +5,7 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::OutputType;
 use embassy_stm32::time::khz;
-use embassy_stm32::timer::complementary_pwm::{ComplementaryPwm, ComplementaryPwmPin};
-use embassy_stm32::timer::simple_pwm::PwmPin;
-use embassy_stm32::timer::Channel;
+use embassy_stm32::timer::{complementary_pwm, Channel};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -16,23 +14,12 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let ch1 = PwmPin::new_ch1(p.PE9, OutputType::PushPull);
-    let ch1n = ComplementaryPwmPin::new_ch1(p.PA7, OutputType::PushPull);
-    let mut pwm = ComplementaryPwm::new(
-        p.TIM1,
-        Some(ch1),
-        Some(ch1n),
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        khz(10),
-        Default::default(),
-    );
+    let mut pwm = complementary_pwm::Builder::new(p.TIM1)
+        .ch1_pin(p.PE9, OutputType::PushPull)
+        .ch1n_pin(p.PA7, OutputType::PushPull)
+        .build(khz(10), Default::default());
 
-    let max = pwm.get_max_duty();
+    let max = pwm.max_duty();
     pwm.set_dead_time(max / 1024);
 
     pwm.enable(Channel::Ch1);
