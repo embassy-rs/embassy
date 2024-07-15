0dd0e593c22dc2ba4fc862ff958663e69b8bafc3
nerwalt <tlawren@gmail.com>
2024-07-15 13:35:21 -0600

    cargo fmt examples

--- examples/nrf9151/s/src/bin/blinky.rs
+++ examples/nrf9151/s/src/bin/blinky.rs
@@ -6,8 +6,6 @@ use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-use defmt;
-
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
