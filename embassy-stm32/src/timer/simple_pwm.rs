f748d238a22d09a4c84b23fa49ca1c6af6503c72
anton smeenk <asmeenk@planet.nl>
2024-07-19 20:51:27 +0200

    Added an update future, to  be able to sync user code with the pmw interrupt

--- embassy-stm32/src/timer/simple_pwm.rs
+++ embassy-stm32/src/timer/simple_pwm.rs
@@ -7,8 +7,13 @@ use embassy_hal_internal::{into_ref, PeripheralRef};
 use super::low_level::{CountingMode, OutputCompareMode, OutputPolarity, Timer};
 use super::{Channel, Channel1Pin, Channel2Pin, Channel3Pin, Channel4Pin, GeneralInstance4Channel};
 use crate::gpio::{AfType, AnyPin, OutputType, Speed};
+use crate::interrupt::typelevel::Interrupt;
+use crate::pac::timer::regs::CntCore;
 use crate::time::Hertz;
 use crate::Peripheral;
+use core::future::Future;
+use core::pin::Pin;
+use core::task::{Context, Poll};
 
 /// Channel 1 marker type.
 pub enum Ch1 {}
@@ -85,7 +90,7 @@ impl<'d, T: GeneralInstance4Channel> SimplePwm<'d, T> {
 
                 this.inner.set_output_compare_preload(channel, true);
             });
-
+        unsafe { T::UpdateInterrupt::enable() };
         this
     }
 
@@ -102,6 +107,10 @@ impl<'d, T: GeneralInstance4Channel> SimplePwm<'d, T> {
     /// Check whether given channel is enabled
     pub fn is_enabled(&self, channel: Channel) -> bool {
         self.inner.get_channel_enable_state(channel)
+    }
+    /// ensable or disable the update interrupt
+    pub fn enable_update_interrupt(&mut self, enable: bool) {
+        self.inner.enable_update_interrupt(enable);
     }
 
     /// Set PWM frequency.
@@ -147,6 +156,21 @@ impl<'d, T: GeneralInstance4Channel> SimplePwm<'d, T> {
     /// Set the output compare mode for a given channel.
     pub fn set_output_compare_mode(&mut self, channel: Channel, mode: OutputCompareMode) {
         self.inner.set_output_compare_mode(channel, mode);
+    }
+    /// Reset the counter value to 0
+    pub fn reset(&self) {
+        self.inner.reset();
+    }
+
+    /// Asynchronously wait until the counter value is updated to zero in the timer
+    /// The update interrupt is enabled by this function
+    pub async fn wait_for_update(&mut self) -> u32 {
+        let regs = unsafe { crate::pac::timer::TimGp16::from_ptr(T::regs()) };
+        regs.dier().modify(|w| w.set_uie(true));
+        UpdateFuture {
+            phantom: PhantomData::<T>,
+        }
+        .await
     }
 
     /// Generate a sequence of PWM waveform
@@ -339,5 +363,43 @@ impl<'d, T: GeneralInstance4Channel> embedded_hal_02::Pwm for SimplePwm<'d, T> {
         P: Into<Self::Time>,
     {
         self.inner.set_frequency(period.into());
+    }
+}
+
+#[must_use = "futures do nothing unless you `.await` or poll them"]
+struct UpdateFuture<T: GeneralInstance4Channel> {
+    phantom: PhantomData<T>,
+}
+
+impl<T: GeneralInstance4Channel> Drop for UpdateFuture<T> {
+    fn drop(&mut self) {
+        critical_section::with(|_| {
+            let regs = unsafe { crate::pac::timer::TimGp16::from_ptr(T::regs()) };
+
+            // disable interrupt enable
+            regs.dier().modify(|w| w.set_uie(false));
+        });
+    }
+}
+
+/// the future will be awoken by the update interrupt. The update flag will be reset,
+/// and the future will return 0 (a dummy value)
+impl<T: GeneralInstance4Channel> Future for UpdateFuture<T> {
+    type Output = u32;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        T::state().up_waker.register(cx.waker());
+        let regs = unsafe { crate::pac::timer::TimGp16::from_ptr(T::regs()) };
+        let sr = regs.sr().read();
+
+        if sr.uif() {
+            // clear the update flag
+            regs.sr().modify(|r| {
+                r.set_uif(false);
+            });
+            Poll::Ready(0)
+        } else {
+            Poll::Pending
+        }
     }
 }
---
+++ examples/stm32g0/src/bin/pwm_update_interrupt.rs
@@ -0,0 +1,102 @@
+#![no_std]
+#![no_main]
+use crate::peripherals::TIM3;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, OutputType, Speed};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::timer::low_level::CountingMode;
+use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
+use embassy_stm32::timer::{self, Channel};
+use embassy_stm32::{bind_interrupts, peripherals};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+// This test is meant for the target nucleo G070 RB
+// On arduino pin d4 (pb5) a pwm signal of  1.0 hz can me measured, with increasing duty cycle
+// On each update interrupt arduino pin d5 (pb4) will be made high for 1 ms.
+// With a logic scope one can measure these signals
+// The user led arduino pin d13 (pa5) will flash with exactly 1 hrz, with duty cycle 20%
+
+bind_interrupts!(
+    struct Irqs {
+      TIM3 => timer::UpdateInterruptHandler<TIM3>;
+    }
+);
+
+#[embassy_executor::task]
+async fn pwm_task(mut pwm_test: PwmTest) {
+    pwm_test.task().await;
+}
+
+pub struct PwmTest {
+    pwm3: SimplePwm<'static, peripherals::TIM3>,
+    led: Output<'static>,
+    d5_pb4: Output<'static>,
+    max3: u32,
+    duty: u32,
+    counter: usize,
+}
+
+impl PwmTest {
+    fn new(mut pwm3: SimplePwm<'static, peripherals::TIM3>, led: Output<'static>, d5_pb4: Output<'static>) -> Self {
+        let max3 = pwm3.get_max_duty();
+        pwm3.reset();
+        pwm3.enable_update_interrupt(true);
+        pwm3.enable(Channel::Ch2);
+        PwmTest {
+            pwm3,
+            max3,
+            duty: 4000,
+            counter: 0,
+            led,
+            d5_pb4,
+        }
+    }
+    async fn task(&mut self) {
+        loop {
+            self.duty = (self.duty + 200) % self.max3;
+            self.pwm3.set_duty(Channel::Ch2, self.duty);
+            // note that the update interrupt will be call exact 100 times per second!
+            self.pwm3.wait_for_update().await;
+            self.counter = (self.counter + 1) % 100;
+            self.d5_pb4.set_high();
+            match self.counter {
+                1 => info!("Update interrupt"),
+                10 => self.led.set_high(),
+                30 => self.led.set_low(),
+
+                _ => (),
+            }
+            Timer::after_millis(1).await;
+            self.d5_pb4.set_low();
+        }
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Testing the update interupt for a simple_pwm instance");
+    let d4_pb5 = PwmPin::new_ch2(p.PB5, OutputType::PushPull);
+    let pwm3 = SimplePwm::<'static>::new(
+        p.TIM3,
+        None,
+        Some(d4_pb5),
+        None,
+        None,
+        Hertz(100),
+        CountingMode::EdgeAlignedUp,
+    );
+    let led_g = Output::new(p.PA5, Level::High, Speed::Low);
+    let d5_pb4 = Output::new(p.PB4, Level::High, Speed::Low);
+    let pwm_test = PwmTest::new(pwm3, led_g, d5_pb4);
+    // note that the pwm_task is the owner of pwm_test.
+    // pwm_test is the owner of the pwm and the led.
+    spawner.spawn(pwm_task(pwm_test)).unwrap();
+
+    loop {
+        Timer::after_secs(10).await;
+        info!("Still alive");
+    }
+}
