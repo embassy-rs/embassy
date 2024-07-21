d4f906dbc77b525ded1839597d0696cda4167990
James Munns <james@onevariable.com>
2024-07-21 13:34:49 +0200

    Remove borrow methods

--- embassy-sync/src/blocking_mutex/mod.rs
+++ embassy-sync/src/blocking_mutex/mod.rs
@@ -54,12 +54,6 @@ impl<T> CriticalSectionMutex<T> {
             inner: BlockingMutex::new(data),
         }
     }
-
-    /// Borrows the data for the duration of the critical section
-    pub fn borrow<'cs>(&'cs self, _cs: critical_section::CriticalSection<'cs>) -> &'cs T {
-        let ptr = unsafe { self.inner.get_unchecked() } as *const T;
-        unsafe { &*ptr }
-    }
 }
 
 impl<T> Deref for NoopMutex<T> {
@@ -89,12 +83,6 @@ impl<T> NoopMutex<T> {
             inner: BlockingMutex::new(data),
         }
     }
-
-    /// Borrows the data
-    pub fn borrow(&self) -> &T {
-        let ptr = unsafe { self.inner.get_unchecked() } as *const T;
-        unsafe { &*ptr }
-    }
 }
 
 // ThreadModeMutex does NOT use the generic mutex from above because it's special:
@@ -107,8 +95,6 @@ impl<T> NoopMutex<T> {
 pub use thread_mode_mutex::*;
 #[cfg(any(cortex_m, feature = "std"))]
 mod thread_mode_mutex {
-    use core::cell::UnsafeCell;
-
     use super::*;
 
     /// A "mutex" that only allows borrowing from thread mode.
@@ -119,7 +105,7 @@ mod thread_mode_mutex {
     ///
     /// On multi-core systems, a `ThreadModeMutex` **is not sufficient** to ensure exclusive access.
     pub struct ThreadModeMutex<T: ?Sized> {
-        inner: UnsafeCell<T>,
+        inner: BlockingMutex<raw::ThreadModeRawMutex, T>,
     }
 
     // NOTE: ThreadModeMutex only allows borrowing from one execution context ever: thread mode.
@@ -132,32 +118,17 @@ mod thread_mode_mutex {
         /// Creates a new mutex
         pub const fn new(value: T) -> Self {
             ThreadModeMutex {
-                inner: UnsafeCell::new(value),
+                inner: BlockingMutex::const_new(raw::ThreadModeRawMutex::new(), value),
             }
         }
     }
 
-    impl<T: ?Sized> ThreadModeMutex<T> {
-        /// Lock the `ThreadModeMutex`, granting access to the data.
-        ///
-        /// # Panics
-        ///
-        /// This will panic if not currently running in thread mode.
-        pub fn lock<R>(&self, f: impl FnOnce(&T) -> R) -> R {
-            f(self.borrow())
-        }
+    impl<T> Deref for ThreadModeMutex<T> {
+        type Target = BlockingMutex<raw::ThreadModeRawMutex, T>;
 
-        /// Borrows the data
-        ///
-        /// # Panics
-        ///
-        /// This will panic if not currently running in thread mode.
-        pub fn borrow(&self) -> &T {
-            assert!(
-                raw::in_thread_mode(),
-                "ThreadModeMutex can only be borrowed from thread mode."
-            );
-            unsafe { &*self.inner.get() }
+        #[inline]
+        fn deref(&self) -> &Self::Target {
+            &self.inner
         }
     }
 
