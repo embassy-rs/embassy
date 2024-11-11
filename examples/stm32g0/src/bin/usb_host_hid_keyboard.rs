85e097fd911922653aed293c3be4573938506850
Ingmar Jager <ijager@users.noreply.github.com>
2024-11-11 11:32:30 +0100

    Implement new UsbChannel for stm32-usb HAL

--- examples/stm32g0/src/bin/usb_host_hid_keyboard.rs
+++ examples/stm32g0/src/bin/usb_host_hid_keyboard.rs
@@ -6,11 +6,14 @@ use embassy_executor::Spawner;
 use embassy_stm32::gpio::{AfType, Level, Output, OutputType, Speed};
 use embassy_stm32::i2c::{self, I2c};
 use embassy_stm32::time::{mhz, Hertz};
-use embassy_stm32::usb::USBHostDriver;
+use embassy_stm32::usb::UsbHost;
 use embassy_stm32::{bind_interrupts, pac, peripherals, usb, Config};
 use embassy_time::Timer;
-use embassy_usb::host::{USBDescriptor, UsbHost};
-use heapless::Vec;
+use embassy_usb::handlers::kbd::KbdHandler;
+use embassy_usb::handlers::UsbHostHandler;
+use embassy_usb::host::UsbHostBusExt;
+use embassy_usb_driver::host::DeviceEvent::Connected;
+use embassy_usb_driver::host::UsbHostDriver;
 use {defmt_rtt as _, panic_probe as _};
 
 pub use crate::pac::rcc::vals::Mcosel;
@@ -112,299 +115,30 @@ async fn main(_spawner: Spawner) {
     debug!("TCPP03-M20 Value 1: {:02X}", read_buf[0]);
 
     // Create the driver, from the HAL.
-    let mut driver = USBHostDriver::new(p.USB, Irqs, p.PA12, p.PA11);
+    let mut usbhost = UsbHost::new(p.USB, Irqs, p.PA12, p.PA11);
 
-    info!("Start USB driver");
-    driver.start();
-    let mut host = UsbHost::new(driver);
+    // info!("Start USB driver");
+    usbhost.start();
+    // let mut host = UsbHost::new(driver);
 
-    host.wait_for_device().await;
-
-    let (_device_desc, cfg_desc) = host.enumerate().await.unwrap();
-
-    info!("Found {} interfaces", cfg_desc.num_interfaces);
-
-    let interface0 = cfg_desc.parse_interface(0).unwrap();
-    info!("Interface 0: {:?}", interface0);
-
-    // let interface1 = cfg_desc.parse_interface(1).unwrap();
-    // info!("Interface 1: {:?}", interface1);
-
-    let endpoints = interface0.parse_endpoints::<4>();
-
-    info!("Endpoints: {:?}", endpoints[0]);
-
-    if hid_keyboard::is_compatible(&interface0) {
-        let hid_desc: hid_keyboard::HIDDescriptor = interface0.parse_class_descriptor().unwrap();
-
-        info!("HID descriptor: {:?}", hid_desc);
-
-        // Request report descriptor
-        let mut buffer = [0u8; 128];
-
-        let request_buffer = &mut buffer[..hid_desc.descriptor_length0 as usize];
-        host.interface_request_descriptor_bytes::<ReportDescriptor>(interface0.interface_number, request_buffer)
-            .await
-            .unwrap();
-
-        if let Ok(mut keyboard) = hid_keyboard::HIDKeyboardBuilder::new(host).build(&cfg_desc).await {
-            info!("Keyboard initialized");
-            loop {
-                keyboard.listen().await;
-            }
+    debug!("Detecting device");
+    // Wait for root-port to detect device
+    let speed = loop {
+        match usbhost.wait_for_device_event().await {
+            Connected(speed) => break speed,
+            _ => {}
         }
-        // let mut host = keyboard.release();
-    } else {
-        host.wait_for_device_disconnect().await;
-    }
+    };
 
-    info!("Bye");
+    println!("Found device with speed = {:?}", speed);
+
+    let enum_info = usbhost.enumerate_root(speed, 1).await.unwrap();
+    let mut kbd = KbdHandler::try_register(&usbhost, enum_info)
+        .await
+        .expect("Couldn't register keyboard");
+
     loop {
-        led.set_high();
-        Timer::after_millis(900).await;
-
-        led.set_low();
-        Timer::after_millis(900).await;
-    }
-}
-
-struct ReportDescriptor {}
-
-impl USBDescriptor for ReportDescriptor {
-    const SIZE: usize = 0; //
-
-    const DESC_TYPE: u8 = 34;
-
-    type Error = ();
-
-    fn try_from_bytes(bytes: &[u8]) -> Result<Self, Self::Error> {
-        debug!("Report descriptor: {:?}", bytes);
-        Ok(Self {})
-    }
-}
-
-mod hid_keyboard {
-
-    use embassy_usb::host::{ConfigurationDescriptor, InterfaceDescriptor, UsbHost};
-    use embassy_usb_driver::host::{ChannelIn, USBHostDriverTrait};
-
-    use super::*;
-
-    pub fn is_compatible(desc: &InterfaceDescriptor) -> bool {
-        desc.interface_class == 3 && desc.interface_subclass == 1 && desc.interface_protocol == 1
-    }
-
-    #[derive(defmt::Format)]
-    pub struct HIDDescriptor {
-        pub len: u8,
-        pub descriptor_type: u8,
-        pub bcd_hid: u16,
-        pub country_code: u8,
-        pub num_descriptors: u8,
-
-        // num_descriptors determines how many pairs of descriptor_typeI/descriptor_lengthI follow.
-        pub descriptor_type0: u8,
-        pub descriptor_length0: u16,
-    }
-
-    impl USBDescriptor for HIDDescriptor {
-        const SIZE: usize = 9; // only valid for 1 descriptor
-
-        const DESC_TYPE: u8 = 33;
-
-        type Error = ();
-
-        fn try_from_bytes(bytes: &[u8]) -> Result<Self, Self::Error> {
-            if bytes.len() < Self::SIZE {
-                return Err(());
-            }
-            if bytes[1] != Self::DESC_TYPE {
-                return Err(());
-            }
-
-            Ok(Self {
-                len: bytes[0],
-                descriptor_type: bytes[1],
-                bcd_hid: u16::from_le_bytes([bytes[2], bytes[3]]),
-                country_code: bytes[4],
-                num_descriptors: bytes[5],
-                descriptor_type0: bytes[6],
-                descriptor_length0: u16::from_le_bytes([bytes[7], bytes[8]]),
-            })
-        }
-    }
-
-    pub struct HIDBootKeyboard<D>
-    where
-        D: USBHostDriverTrait,
-    {
-        host: UsbHost<D>,
-        channel: D::ChannelIn,
-    }
-
-    impl<D> HIDBootKeyboard<D>
-    where
-        D: USBHostDriverTrait,
-    {
-        pub async fn listen(&mut self) {
-            let mut buffer = [0u8; 8];
-
-            if let Ok(_l) = self.channel.read(&mut buffer[..]).await {
-                let keycodes = parse_payload(&buffer);
-
-                for keycode in keycodes {
-                    let chr = keycode_to_ascii(keycode);
-                    info!("Ascii: {:?}", chr);
-                }
-            }
-        }
-
-        #[allow(unused)]
-        pub fn release(self) -> UsbHost<D> {
-            self.host
-        }
-    }
-
-    pub struct HIDKeyboardBuilder<D>
-    where
-        D: USBHostDriverTrait,
-    {
-        host: UsbHost<D>,
-    }
-
-    impl<D: USBHostDriverTrait> HIDKeyboardBuilder<D> {
-        pub fn new(host: UsbHost<D>) -> Self {
-            Self { host }
-        }
-
-        pub async fn build(mut self, cfg_desc: &ConfigurationDescriptor) -> Result<HIDBootKeyboard<D>, UsbHost<D>> {
-            // Since we know this is a boot keyboard we don't need to parse the report descriptor.
-
-            // We still need an Endpoint Descriptor.
-            // claim endpoint
-
-            debug!("Initializing HID keyboard");
-
-            let mut boot_hid_interface = None;
-            for i in 0..cfg_desc.num_interfaces {
-                let interface = cfg_desc.parse_interface(i as usize).unwrap();
-                if is_compatible(&interface) {
-                    boot_hid_interface = Some(interface);
-                }
-            }
-
-            let Some(interface) = boot_hid_interface else {
-                error!("No HID interface found");
-                return Err(self.host);
-            };
-
-            const SET_PROTOCOL: u8 = 0x0B;
-            const BOOT_PROTOCOL: u16 = 0x0000;
-            self.host
-                .class_request_out(SET_PROTOCOL, BOOT_PROTOCOL, interface.interface_number as u16, &mut [])
-                .await
-                .unwrap();
-
-            const SET_IDLE: u8 = 0x0A;
-            self.host
-                .class_request_out(SET_IDLE, 0, interface.interface_number as u16, &mut [])
-                .await
-                .unwrap();
-
-            let endpoints = interface.parse_endpoints::<1>();
-
-            if endpoints.len() != 1 {
-                error!("Wrong number of endpoints");
-                return Err(self.host);
-            }
-
-            let channel = self.host.claim_endpoint(&endpoints[0]).unwrap();
-
-            Ok(HIDBootKeyboard {
-                host: self.host,
-                channel: channel,
-            })
-        }
-    }
-
-    fn parse_payload(buffer: &[u8; 8]) -> Vec<u8, 6> {
-        let _modifier = buffer[0];
-        // byte 1 is reserved
-
-        let key_codes = buffer[2..]
-            .iter()
-            .filter(|b| **b != 0)
-            .map(|b| *b)
-            .collect::<Vec<u8, 6>>();
-
-        return key_codes;
-    }
-
-    const SCANCODE_MAP: [char; 57] = [
-        ' ',  // 0 Reserved (no event)
-        ' ',  // 1
-        ' ',  // 2
-        ' ',  // 3
-        'a',  // 4
-        'b',  // 5
-        'c',  // 6
-        'd',  // 7
-        'e',  // 8
-        'f',  // 9
-        'g',  // 10
-        'h',  // 11
-        'i',  // 12
-        'j',  // 13
-        'k',  // 14
-        'l',  // 15
-        'm',  // 16
-        'n',  // 17
-        'o',  // 18
-        'p',  // 19
-        'q',  // 20
-        'r',  // 21
-        's',  // 22
-        't',  // 23
-        'u',  // 24
-        'v',  // 25
-        'w',  // 26
-        'x',  // 27
-        'y',  // 28
-        'z',  // 29
-        '1',  // 30
-        '2',  // 31
-        '3',  // 32
-        '4',  // 33
-        '5',  // 34
-        '6',  // 35
-        '7',  // 36
-        '8',  // 37
-        '9',  // 38
-        '0',  // 39
-        '\n', // 40 enter
-        ' ',  // 41 escape
-        ' ',  // 42 backspace
-        '\t', // 43 tab
-        ' ',  // 44 space
-        '-',  // 45 minus
-        '=',  // 46 equal
-        '[',  // 47 left bracket
-        ']',  // 48 right bracket
-        '\\', // 49 backslash
-        '#',  // 50 pound
-        ';',  // 51 semicolon
-        '\'', // 52 apostrophe
-        '`',  // 53 grave accent
-        ',',  // 54 comma
-        '.',  // 55 period
-        '/',  // 56 slash
-    ];
-
-    fn keycode_to_ascii(keycode: u8) -> char {
-        if keycode < SCANCODE_MAP.len() as u8 {
-            SCANCODE_MAP[keycode as usize]
-        } else {
-            0 as char
-        }
+        let result = kbd.wait_for_event().await;
+        debug!("{}", result);
     }
 }
