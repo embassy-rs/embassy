487ad4f9c95a3d2dd11e203ecd152ebb40e82d2c
Dániel Buga <bugadani@gmail.com>
2024-11-26 09:53:55 +0100

    Separate claimed and spawned states

--- embassy-executor/src/raw/state_atomics.rs
+++ embassy-executor/src/raw/state_atomics.rs
@@ -1,12 +1,14 @@
 use core::sync::atomic::{AtomicU32, Ordering};
 
+/// Task is claimed (its storage is in use)
+pub(crate) const STATE_CLAIMED: u32 = 1 << 0;
 /// Task is spawned (has a future)
-pub(crate) const STATE_SPAWNED: u32 = 1 << 0;
+pub(crate) const STATE_SPAWNED: u32 = 1 << 1;
 /// Task is in the executor run queue
-pub(crate) const STATE_RUN_QUEUED: u32 = 1 << 1;
+pub(crate) const STATE_RUN_QUEUED: u32 = 1 << 2;
 /// Task is in the executor timer queue
 #[cfg(feature = "integrated-timers")]
-pub(crate) const STATE_TIMER_QUEUED: u32 = 1 << 2;
+pub(crate) const STATE_TIMER_QUEUED: u32 = 1 << 3;
 
 pub(crate) struct State {
     state: AtomicU32,
@@ -19,18 +21,29 @@ impl State {
         }
     }
 
-    /// If task is idle, mark it as spawned + run_queued and return true.
+    /// If task is idle, mark it as claimed and return true.
     #[inline(always)]
-    pub fn spawn(&self) -> bool {
+    pub fn claim(&self) -> bool {
         self.state
-            .compare_exchange(0, STATE_SPAWNED | STATE_RUN_QUEUED, Ordering::AcqRel, Ordering::Acquire)
+            .compare_exchange(0, STATE_CLAIMED, Ordering::AcqRel, Ordering::Acquire)
             .is_ok()
+    }
+
+    /// Mark a claimed task ready to run.
+    ///
+    /// # Safety
+    ///
+    /// The task must be claimed, its executor must be configured. This function must
+    /// not be called when the task is already spawned.
+    #[inline(always)]
+    pub unsafe fn mark_spawned(&self) {
+        self.state.store(STATE_SPAWNED | STATE_RUN_QUEUED, Ordering::Release);
     }
 
     /// Unmark the task as spawned.
     #[inline(always)]
     pub fn despawn(&self) {
-        self.state.fetch_and(!STATE_SPAWNED, Ordering::AcqRel);
+        self.state.fetch_and(!(STATE_SPAWNED | STATE_CLAIMED), Ordering::AcqRel);
     }
 
     /// Mark the task as run-queued if it's spawned and isn't already run-queued. Return true on success.
