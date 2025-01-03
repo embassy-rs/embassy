d9ef2c94d70adb94d77bee184c852e0e3d186f97
elagil <elagil@takanome.de>
2025-01-03 17:54:59 +0100

    chore: clean up stm32h5 ucpd

--- embassy-stm32/src/ucpd.rs
+++ embassy-stm32/src/ucpd.rs
@@ -299,24 +299,18 @@ impl<'d, T: Instance> CcPhy<'d, T> {
         #[cfg(stm32h5)]
         T::REGS.cfgr3().modify(|w| match cc_pull {
             CcPull::Source1_5A => {
-                #[cfg(stm32h5)]
-                {
-                    let trim_1a5_cc1 = unsafe { *(0x08FF_F844 as *const u32) & 0xF };
-                    let trim_1a5_cc2 = unsafe { ((*(0x08FF_F844 as *const u32)) >> 16) & 0xF };
+                let trim_1a5_cc1 = unsafe { *(0x08FF_F844 as *const u32) & 0xF };
+                let trim_1a5_cc2 = unsafe { ((*(0x08FF_F844 as *const u32)) >> 16) & 0xF };
 
-                    w.set_trim_cc1_rp(trim_1a5_cc1 as u8);
-                    w.set_trim_cc2_rp(trim_1a5_cc2 as u8);
-                };
+                w.set_trim_cc1_rp(trim_1a5_cc1 as u8);
+                w.set_trim_cc2_rp(trim_1a5_cc2 as u8);
             }
             _ => {
-                #[cfg(stm32h5)]
-                {
-                    let trim_3a0_cc1 = unsafe { (*(0x4002_242C as *const u32) >> 4) & 0xF };
-                    let trim_3a0_cc2 = unsafe { ((*(0x4002_242C as *const u32)) >> 12) & 0xF };
+                let trim_3a0_cc1 = unsafe { (*(0x4002_242C as *const u32) >> 4) & 0xF };
+                let trim_3a0_cc2 = unsafe { ((*(0x4002_242C as *const u32)) >> 12) & 0xF };
 
-                    w.set_trim_cc1_rp(trim_3a0_cc1 as u8);
-                    w.set_trim_cc2_rp(trim_3a0_cc2 as u8);
-                };
+                w.set_trim_cc1_rp(trim_3a0_cc1 as u8);
+                w.set_trim_cc2_rp(trim_3a0_cc2 as u8);
             }
         });
 
