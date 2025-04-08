0a410e6ee627cd8d13851a7ee0536412632e3265
James Munns <james@onevariable.com>
2025-04-08 10:11:07 +0200

    One more must_use

--- embassy-executor/src/raw/mod.rs
+++ embassy-executor/src/raw/mod.rs
@@ -31,17 +31,15 @@ use core::marker::PhantomData;
 use core::mem;
 use core::pin::Pin;
 use core::ptr::NonNull;
-use core::ptr::addr_of_mut;
 #[cfg(not(feature = "arch-avr"))]
 use core::sync::atomic::AtomicPtr;
 use core::sync::atomic::Ordering;
 use core::task::{Context, Poll};
 
-#[cfg(feature = "arch-avr")]
-use portable_atomic::AtomicPtr;
-
 #[cfg(feature = "drs-scheduler")]
 pub use deadline::Deadline;
+#[cfg(feature = "arch-avr")]
+use portable_atomic::AtomicPtr;
 
 use self::run_queue::{RunQueue, RunQueueItem};
 use self::state::State;
