55c01d6388ef49eda39a29ccfee687daab6ba6db
ferris <ferris@devdroplets.com>
2025-04-09 13:20:51 +0200

    Add missing Synopsys registers

--- embassy-usb-synopsys-otg/src/lib.rs
+++ embassy-usb-synopsys-otg/src/lib.rs
@@ -1099,7 +1099,7 @@ impl<'d> embassy_usb_driver::EndpointOut for Endpoint<'d, Out> {
                             if frame_is_odd {
                                 r.set_sd0pid_sevnfrm(true);
                             } else {
-                                r.set_sd1pid_soddfrm(true);
+                                r.set_soddfrm(true);
                             }
                         });
                     }
@@ -1201,7 +1201,7 @@ impl<'d> embassy_usb_driver::EndpointIn for Endpoint<'d, In> {
                     if frame_is_odd {
                         r.set_sd0pid_sevnfrm(true);
                     } else {
-                        r.set_sd1pid_soddfrm(true);
+                        r.set_soddfrm_sd1pid(true);
                     }
                 });
             }
