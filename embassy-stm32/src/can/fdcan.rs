30fa4fd77be3bb43e540f775f7147b0a63aada87
Kevin Lannen <kevin@luci.com>
2025-04-11 11:59:15 -0600

    fdcan: Add fn to set tx buffer mode

--- embassy-stm32/src/can/fdcan.rs
+++ embassy-stm32/src/can/fdcan.rs
@@ -245,6 +245,11 @@ impl<'d> CanConfigurator<'d> {
         self.config = self.config.set_nominal_bit_timing(nbtr);
     }
 
+    /// Configures the transmit buffer to FIFO or priority mode.
+    pub fn set_tx_buffer_mode(&mut self, mode: crate::can::fd::config::TxBufferMode) {
+        self.config.tx_buffer_mode = mode;
+    }
+
     /// Configures the bit timings for VBR data calculated from supplied bitrate. This also sets confit to allow can FD and VBR
     pub fn set_fd_data_bitrate(&mut self, bitrate: u32, transceiver_delay_compensation: bool) {
         let bit_timing = util::calc_can_timings(self.periph_clock, bitrate).unwrap();
