0e28ba1091257111f71b76a664d7038dbfcf9b5e
James Munns <james@onevariable.com>
2025-04-16 15:59:28 +0200

    "Deadline Rank Sorted Scheduler" -> "Earliest Deadline First Scheduler"

--- embassy-executor/src/raw/mod.rs
+++ embassy-executor/src/raw/mod.rs
@@ -28,7 +28,7 @@ pub(crate) mod util;
 #[cfg_attr(feature = "turbowakers", path = "waker_turbo.rs")]
 mod waker;
 
-#[cfg(feature = "drs-scheduler")]
+#[cfg(feature = "edf-scheduler")]
 mod deadline;
 
 use core::future::Future;
@@ -45,7 +45,7 @@ use embassy_executor_timer_queue::TimerQueueItem;
 #[cfg(feature = "arch-avr")]
 use portable_atomic::AtomicPtr;
 
-#[cfg(feature = "drs-scheduler")]
+#[cfg(feature = "edf-scheduler")]
 pub use deadline::Deadline;
 #[cfg(feature = "arch-avr")]
 use portable_atomic::AtomicPtr;
@@ -105,9 +105,9 @@ pub(crate) struct TaskHeader {
     pub(crate) state: State,
     pub(crate) run_queue_item: RunQueueItem,
 
-    /// Deadline Rank Scheduler Deadline. This field should not be accessed outside the context of
-    /// the task itself as it being polled by the executor.
-    #[cfg(feature = "drs-scheduler")]
+    /// Earliest Deadline First scheduler Deadline. This field should not be accessed
+    /// outside the context of the task itself as it being polled by the executor.
+    #[cfg(feature = "edf-scheduler")]
     pub(crate) deadline: SyncUnsafeCell<u64>,
 
     pub(crate) executor: AtomicPtr<SyncExecutor>,
@@ -216,7 +216,7 @@ impl<F: Future + 'static> TaskStorage<F> {
                 run_queue_item: RunQueueItem::new(),
                 // NOTE: The deadline is set to zero to allow the initializer to reside in `.bss`. This
                 // will be lazily initalized in `initialize_impl`
-                #[cfg(feature = "drs-scheduler")]
+                #[cfg(feature = "edf-scheduler")]
                 deadline: SyncUnsafeCell::new(0u64),
                 executor: AtomicPtr::new(core::ptr::null_mut()),
                 // Note: this is lazily initialized so that a static `TaskStorage` will go in `.bss`
@@ -316,7 +316,7 @@ impl<F: Future + 'static> AvailableTask<F> {
 
             // By default, deadlines are set to the maximum value, so that any task WITH
             // a set deadline will ALWAYS be scheduled BEFORE a task WITHOUT a set deadline
-            #[cfg(feature = "drs-scheduler")]
+            #[cfg(feature = "edf-scheduler")]
             self.task.raw.deadline.set(deadline::Deadline::UNSET_DEADLINE_TICKS);
 
             let task = TaskRef::new(self.task);
