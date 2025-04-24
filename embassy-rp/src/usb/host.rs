452243142be2fe915143cdbd0c5e3a30893b3285
Łukasz Dobrowolski <lukasz@dobrowolski.io>
2025-04-24 22:34:18 +0200

    Fix channel stall
    0.000431 DEBUG Detecting device
    └─ usb_host_keyboard::____embassy_main_task::{async_fn#0} @ src/bin/usb_host_keyboard.rs:25
    Found device with speed = Low
    0.161293 ERROR panicked at src/bin/usb_host_keyboard.rs:36:65:
    called `Result::unwrap()` on an `Err` value: ChannelError(Stall)

--- embassy-rp/src/usb/host.rs
+++ embassy-rp/src/usb/host.rs
@@ -586,7 +586,9 @@ impl<'d, T: Instance, E: channel::Type, D: channel::Direction> UsbChannel<E, D> 
         self.send_setup(setup).await?;
 
         // Data stage
-        self.request_out(&buf[..setup.length as usize]).await?;
+        if setup.length > 0 {
+            self.request_out(&buf[..setup.length as usize]).await?;
+        }
 
         // Status stage
         self.control_status(true).await?;
