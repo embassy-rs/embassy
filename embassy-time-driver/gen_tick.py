34b6a518f87a528856f1891d32ac1a48d6239b18
Curly <u007d@humanenginuity.com>
2025-04-30 07:50:13 -0700

    add 133MHz tick rate to support PR2040 @ 133MHz when `TIMERx`'s `SOURCE` is set to `SYSCLK`

--- embassy-time-driver/gen_tick.py
+++ embassy-time-driver/gen_tick.py
@@ -22,6 +22,7 @@ for i in range(1, 30):
     ticks.append(10 * i * 1_000_000)
 for i in range(15, 50):
     ticks.append(20 * i * 1_000_000)
+ticks.append(133 * 1_000_000)
 
 seen = set()
 ticks = sorted([x for x in ticks if not (x in seen or seen.add(x))])
