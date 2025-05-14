3839baf7d264645845b7e3c7c660b07af7eb4c73
Alix ANNERAUD <alix@anneraud.fr>
2025-05-14 18:53:14 +0200

    Refactor test macro to use async task execution

--- embassy-executor-macros/src/macros/test.rs
+++ embassy-executor-macros/src/macros/test.rs
@@ -1,16 +1,26 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, ItemFn};
 
 pub fn test(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(input as ItemFn);
     let fn_name = &input_fn.sig.ident;
 
+    let task_name = format_ident!("__{}_task", fn_name);
+
     let gen = quote! {
+        #[::embassy_executor::task()]
+        #[allow(clippy::future_not_send)]
+        async fn #task_name() {
+            #input_fn
+        }
+
+
         #[test]
         fn #fn_name() {
-            embassy_executor::run(|spawner| async move {
-                #input_fn
+            let mut executor = ::embassy_executor::Executor::new();
+            executor.run(|spawner| {
+                spawner.spawn(#task_name()).unwrap();
             });
         }
     };
