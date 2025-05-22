e671aff92d4e78be22a3e0a01af03d60c944c526
Łukasz Dobrowolski <lukasz@dobrowolski.io>
2025-05-22 05:03:32 +0200

    embassy-time: Hide embedded-hal 0.2 behind a feature

--- embassy-time/src/delay.rs
+++ embassy-time/src/delay.rs
@@ -44,36 +44,42 @@ impl embedded_hal_async::delay::DelayNs for Delay {
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayMs<u8> for Delay {
     fn delay_ms(&mut self, ms: u8) {
         block_for(Duration::from_millis(ms as u64))
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayMs<u16> for Delay {
     fn delay_ms(&mut self, ms: u16) {
         block_for(Duration::from_millis(ms as u64))
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayMs<u32> for Delay {
     fn delay_ms(&mut self, ms: u32) {
         block_for(Duration::from_millis(ms as u64))
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayUs<u8> for Delay {
     fn delay_us(&mut self, us: u8) {
         block_for(Duration::from_micros(us as u64))
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayUs<u16> for Delay {
     fn delay_us(&mut self, us: u16) {
         block_for(Duration::from_micros(us as u64))
     }
 }
 
+#[cfg(feature = "embedded-hal-02")]
 impl embedded_hal_02::blocking::delay::DelayUs<u32> for Delay {
     fn delay_us(&mut self, us: u32) {
         block_for(Duration::from_micros(us as u64))
