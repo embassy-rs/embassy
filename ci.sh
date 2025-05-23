489c9df453b86f80f040fe1e48d89ab342e24b3a
Ulf Lilleengen <ulf@digili.no>
2025-05-23 14:32:36 +0200

    chore: temporarily disable pico tests

--- ci.sh
+++ ci.sh
@@ -362,6 +362,8 @@ rm out/tests/pimoroni-pico-plus-2/flash
 rm out/tests/pimoroni-pico-plus-2/i2c
 # The pico2 plus doesn't have the adcs hooked up like the picoW does.
 rm out/tests/pimoroni-pico-plus-2/adc
+# temporarily disabled, bad hardware connection.
+rm -f out/tests/rpi-pico/*
 
 if [[ -z "${TELEPROBE_TOKEN-}" ]]; then
     echo No teleprobe token found, skipping running HIL tests
