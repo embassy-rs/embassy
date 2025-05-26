ca17b41d0dfa32150ac72187e1b05924577641cb
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-05-26 14:43:21 +0200

    fixup! feat(stm32-g0): provide a `const` constructor on `rcc::Config`

--- embassy-stm32/src/rcc/g0.rs
+++ embassy-stm32/src/rcc/g0.rs
@@ -98,7 +98,6 @@ pub struct Config {
 }
 
 impl Config {
-    #[inline]
     pub const fn new() -> Self {
         Config {
             hsi: Some(Hsi {
@@ -120,7 +119,6 @@ impl Config {
 }
 
 impl Default for Config {
-    #[inline]
     fn default() -> Config {
         Self::new()
     }
