3a432920978dc48f0b4c0a8da7c118415a0708fd
Melvin Wang <melvin.mc.wang@gmail.com>
2025-06-18 16:16:06 -0700

    commit expected errors

--- embassy-sync/tests/ui/sync_impl/lazy_lock_function.rs
+++ embassy-sync/tests/ui/sync_impl/lazy_lock_function.rs
@@ -3,13 +3,9 @@ use embassy_sync::lazy_lock::LazyLock;
 fn main() {
     let x = 128u8;
     let x_ptr: *const u8 = core::ptr::addr_of!(x);
+    let closure_capturing_non_sync_variable = || unsafe { core::ptr::read(x_ptr) };
 
-    let closure_capturing_non_sync_variable = || {
-        unsafe {
-            core::ptr::read(x_ptr)
-        }
-    };
-
-    // This should fail to compile because the closure captures a non-Sync variable: x_ptr.
-    let _lazy_u8: LazyLock<u8, _> = LazyLock::new(closure_capturing_non_sync_variable);
+    check_sync(LazyLock::new(closure_capturing_non_sync_variable));
 }
+
+fn check_sync<T: Sync>(_lazy_lock: T) {}
