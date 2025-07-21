b3112c17d91e67bfb4ce87ec34a6ac792ce66fd3
Siarhei B <sbespanski@crispaudio.de>
2025-07-21 14:22:32 +0200

    mspm0: Add I2C Controller examples for mspm0l1306, mspm0g3507 MCUs
    - mspm0l1306 examples: add I2C blocking & async examples
    - mspm0l1306 examples: add -O2 optimization due to Flash limitations
    - mspm0g3507 examples: add I2C blocking & async examples

---
+++ examples/mspm0g3507/src/bin/i2c_async.rs
@@ -0,0 +1,43 @@
+//! Example of using async I2C
+//!
+//! This uses the virtual COM port provided on the LP-MSPM0G3507 board.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_mspm0::bind_interrupts;
+use embassy_mspm0::i2c::{BusSpeed, ClockSel, Config, I2c, InterruptHandler};
+use embassy_mspm0::peripherals::I2C1;
+use {defmt_rtt as _, panic_halt as _};
+
+const ADDRESS: u8 = 0x6a;
+
+bind_interrupts!(struct Irqs {
+    I2C1 => InterruptHandler<I2C1>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let p = embassy_mspm0::init(Default::default());
+
+    let instance = p.I2C1;
+    let scl = p.PB2;
+    let sda = p.PB3;
+
+    let mut config = Config::default();
+    config.clock_source = ClockSel::BusClk;
+    config.bus_speed = BusSpeed::FastMode;
+    let mut i2c = unwrap!(I2c::new_async(instance, scl, sda, Irqs, config));
+
+    let mut to_read = [0u8; 1];
+    let to_write: u8 = 0x0F;
+
+    match i2c.async_write_read(ADDRESS, &[to_write], &mut to_read).await {
+        Ok(()) => info!("Register {}: {}", to_write, to_read[0]),
+        Err(e) => error!("I2c Error: {:?}", e),
+    }
+
+    loop {}
+}
---
+++ examples/mspm0l1306/src/bin/i2c.rs
@@ -0,0 +1,37 @@
+//! Example of using blocking I2C
+//!
+//! This uses the virtual COM port provided on the LP-MSPM0L1306 board.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_mspm0::i2c::{BusSpeed, ClockSel, Config, I2c};
+use {defmt_rtt as _, panic_halt as _};
+
+const ADDRESS: u8 = 0x6a;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let p = embassy_mspm0::init(Default::default());
+
+    let instance = p.I2C0;
+    let scl = p.PA1;
+    let sda = p.PA0;
+
+    let mut config = Config::default();
+    config.clock_source = ClockSel::BusClk;
+    config.bus_speed = BusSpeed::FastMode;
+    let mut i2c = unwrap!(I2c::new_blocking(instance, scl, sda, config));
+
+    let mut to_read = [0u8; 1];
+    let to_write: u8 = 0x0F;
+
+    match i2c.blocking_write_read(ADDRESS, &[to_write], &mut to_read) {
+        Ok(()) => info!("Register {}: {}", to_write, to_read[0]),
+        Err(e) => error!("I2c Error: {:?}", e),
+    }
+
+    loop {}
+}
