181dee6f3535f762d31a1085df73607288e7196d
Tommaso Clini <tommasoclini@gmail.com>
2025-08-19 12:56:32 +0200

    SeqReader struct to avoid writing to the sequence register every reading

--- embassy-stm32/src/adc/v2.rs
+++ embassy-stm32/src/adc/v2.rs
@@ -200,6 +200,116 @@ where
         self.convert()
     }
 
+    /*
+    let request = rx_dma.request();
+            let transfer = unsafe {
+                Transfer::new_read(
+                    rx_dma,
+                    request,
+                    r.dr().as_ptr() as *mut u16,
+                    readings,
+                    Default::default(),
+                )
+            };
+
+            r.cr2().modify(|w| w.set_swstart(true));
+
+            transfer.await;
+
+            // Clean up: disable DMA
+            r.cr2().modify(|w| {
+                w.set_dma(false);
+            });
+
+            r.cr1().modify(|w| {
+                w.set_scan(false);
+            });
+
+            // Clear any remaining status flags
+            r.sr().modify(|reg| {
+                reg.set_eoc(false);
+                reg.set_ovr(false);
+                reg.set_strt(false);
+            });
+        */
+
+    pub fn seq_reader<'reader, 'func, RX_DMA: RxDma<T>>(
+        &'reader mut self,
+        rx_dma: Peri<'reader, RX_DMA>,
+        sequence: impl ExactSizeIterator<Item = (&'func mut AnyAdcChannel<T>, SampleTime)>,
+        readings: &'reader mut [u16],
+    ) -> Result<SeqReader<'reader, 'd, T, RX_DMA>, ()> {
+        if !self.is_on() {
+            self.turn(true);
+        }
+
+        let len = sequence.len();
+
+        if len > 16 || len != readings.len() {
+            return Err(());
+        }
+
+        let r = T::regs();
+
+        r.sqr1().modify(|w| {
+            w.set_l(len as u8);
+        });
+
+        // Configure channels and ranks
+        for (i, (channel, sample_time)) in sequence.enumerate() {
+            Self::set_channel_sample_time(channel.channel(), sample_time);
+            let ch = channel.channel();
+            match i {
+                0 => r.sqr3().modify(|w| w.set_sq(0, ch)),
+                1 => r.sqr3().modify(|w| w.set_sq(1, ch)),
+                2 => r.sqr3().modify(|w| w.set_sq(2, ch)),
+                3 => r.sqr3().modify(|w| w.set_sq(3, ch)),
+                4 => r.sqr3().modify(|w| w.set_sq(4, ch)),
+                5 => r.sqr3().modify(|w| w.set_sq(5, ch)),
+                6 => r.sqr2().modify(|w| w.set_sq(0, ch)),
+                7 => r.sqr2().modify(|w| w.set_sq(1, ch)),
+                8 => r.sqr2().modify(|w| w.set_sq(2, ch)),
+                9 => r.sqr2().modify(|w| w.set_sq(3, ch)),
+                10 => r.sqr2().modify(|w| w.set_sq(4, ch)),
+                11 => r.sqr2().modify(|w| w.set_sq(5, ch)),
+                12 => r.sqr1().modify(|w| w.set_sq(0, ch)),
+                13 => r.sqr1().modify(|w| w.set_sq(1, ch)),
+                14 => r.sqr1().modify(|w| w.set_sq(2, ch)),
+                15 => r.sqr1().modify(|w| w.set_sq(3, ch)),
+                _ => unreachable!(),
+            };
+        }
+
+        r.sr().modify(|reg| {
+            reg.set_eoc(false);
+            reg.set_ovr(false);
+            reg.set_strt(false);
+        });
+
+        r.cr1().modify(|w| {
+            // Enable interrupt for end of conversion
+            // w.set_eocie(true);
+            // Enable interrupt for overrun
+            // w.set_ovrie(true);
+            // Scanning converisons of multiple channels
+            w.set_scan(true);
+            // Continuous conversion mode
+            w.set_discen(false);
+        });
+
+        r.cr2().modify(|w| {
+            // Enable DMA mode
+            w.set_dma(true);
+            // Disable continuous conversions
+            w.set_cont(false);
+            w.set_dds(vals::Dds::CONTINUOUS);
+            // EOC flag is set at the end of each conversion.
+            w.set_eocs(vals::Eocs::EACH_CONVERSION);
+        });
+
+        Ok(SeqReader::new(self, readings, rx_dma))
+    }
+
     pub async fn read_seq(
         &mut self,
         rx_dma: Peri<'_, impl RxDma<T>>,
@@ -315,6 +425,52 @@ where
         } else {
             T::regs().smpr1().modify(|reg| reg.set_smp((ch - 10) as _, sample_time));
         }
+    }
+}
+
+pub struct SeqReader<'a, 'd, T: Instance, RX_DMA: RxDma<T>> {
+    adc: &'a mut Adc<'d, T>,
+    buf: &'a mut [u16],
+    rx_dma: Peri<'a, RX_DMA>,
+}
+
+impl<'a, 'd, T: Instance, RX_DMA: RxDma<T>> SeqReader<'a, 'd, T, RX_DMA> {
+    fn new(adc: &'a mut Adc<'d, T>, buf: &'a mut [u16], rx_dma: Peri<'a, RX_DMA>) -> Self {
+        Self { adc, buf, rx_dma }
+    }
+
+    pub async fn read(&mut self) -> Result<&[u16], ()> {
+        let r = T::regs();
+
+        r.sr().modify(|reg| {
+            reg.set_eoc(false);
+            reg.set_ovr(false);
+            reg.set_strt(false);
+        });
+
+        let request = self.rx_dma.request();
+        let transfer = unsafe {
+            Transfer::new_read(
+                self.rx_dma.reborrow(),
+                request,
+                r.dr().as_ptr() as *mut u16,
+                &mut self.buf,
+                Default::default(),
+            )
+        };
+
+        r.cr2().modify(|w| w.set_swstart(true));
+
+        transfer.await;
+
+        // Clear any remaining status flags
+        r.sr().modify(|reg| {
+            reg.set_eoc(false);
+            reg.set_ovr(false);
+            reg.set_strt(false);
+        });
+
+        Ok(&self.buf[..])
     }
 }
 
