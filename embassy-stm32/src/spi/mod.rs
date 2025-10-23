2970cbbe516524a57055d272dbfc0c8ac98e1361
Cristian Milatinov <milatinovcristian@gmail.com>
2025-10-22 20:01:16 -0400

    fix: added spi_v6 conditional compilation feature

--- embassy-stm32/src/spi/mod.rs
+++ embassy-stm32/src/spi/mod.rs
@@ -136,10 +136,10 @@ pub mod mode {
     #[allow(private_bounds)]
     pub trait CommunicationMode: SealedMode {
         /// Spi communication mode
-        #[cfg(not(any(spi_v4, spi_v5)))]
+        #[cfg(not(any(spi_v4, spi_v5, spi_v6)))]
         const MASTER: vals::Mstr;
         /// Spi communication mode
-        #[cfg(any(spi_v4, spi_v5))]
+        #[cfg(any(spi_v4, spi_v5, spi_v6))]
         const MASTER: vals::Master;
     }
 
@@ -150,17 +150,17 @@ pub mod mode {
 
     impl SealedMode for Master {}
     impl CommunicationMode for Master {
-        #[cfg(not(any(spi_v4, spi_v5)))]
+        #[cfg(not(any(spi_v4, spi_v5, spi_v6)))]
         const MASTER: vals::Mstr = vals::Mstr::MASTER;
-        #[cfg(any(spi_v4, spi_v5))]
+        #[cfg(any(spi_v4, spi_v5, spi_v6))]
         const MASTER: vals::Master = vals::Master::MASTER;
     }
 
     impl SealedMode for Slave {}
     impl CommunicationMode for Slave {
-        #[cfg(not(any(spi_v4, spi_v5)))]
+        #[cfg(not(any(spi_v4, spi_v5, spi_v6)))]
         const MASTER: vals::Mstr = vals::Mstr::SLAVE;
-        #[cfg(any(spi_v4, spi_v5))]
+        #[cfg(any(spi_v4, spi_v5, spi_v6))]
         const MASTER: vals::Master = vals::Master::SLAVE;
     }
 }
