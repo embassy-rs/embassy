83deef3672c651ac86e7ad497eccc52119b3a27f
Ulf Lilleengen <ulf@digili.no>
2025-10-28 14:38:02 +0100

    chore: simplify spim example to be testable on devkit

--- examples/nrf54l15/src/bin/spim.rs
+++ examples/nrf54l15/src/bin/spim.rs
@@ -1,9 +1,7 @@
 #![no_std]
 #![no_main]
 
-use defmt::{info, unwrap};
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
 use embassy_nrf::{bind_interrupts, peripherals, spim};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -14,59 +12,19 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    info!("running!");
-
     let mut config = spim::Config::default();
-    config.frequency = spim::Frequency::M16;
+    config.frequency = spim::Frequency::M32;
+    let mut spim = spim::Spim::new(p.SERIAL00, Irqs, p.P2_05, p.P2_09, p.P2_08, config.clone());
+    let data = [
+        0x42, 0x43, 0x44, 0x45, 0x66, 0x12, 0x23, 0x34, 0x45, 0x19, 0x91, 0xaa, 0xff, 0xa5, 0x5a, 0x77,
+    ];
+    let mut buf = [0u8; 16];
 
-    let mut spim = spim::Spim::new(p.SERIAL00, Irqs, p.P0_01, p.P0_00, p.P0_02, config);
+    buf.fill(0);
+    spim.blocking_transfer(&mut buf, &data).unwrap();
+    assert_eq!(data, buf);
 
-    // Example on how to talk to an ADXL343
-
-    let mut ncs = Output::new(p.P0_03, Level::High, OutputDrive::Standard);
-
-    // Example on how to talk to an ENC28J60 chip
-
-    // softreset
-    cortex_m::asm::delay(10);
-    ncs.set_low();
-    cortex_m::asm::delay(5);
-    let tx = [0xFF];
-    unwrap!(spim.transfer(&mut [], &tx).await);
-    cortex_m::asm::delay(10);
-    ncs.set_high();
-
-    cortex_m::asm::delay(100000);
-
-    let mut rx = [0; 2];
-
-    // read ESTAT
-    cortex_m::asm::delay(5000);
-    ncs.set_low();
-    cortex_m::asm::delay(5000);
-    let tx = [0b000_11101, 0];
-    unwrap!(spim.transfer(&mut rx, &tx).await);
-    cortex_m::asm::delay(5000);
-    ncs.set_high();
-    info!("estat: {=[?]}", rx);
-
-    // Switch to bank 3
-    cortex_m::asm::delay(10);
-    ncs.set_low();
-    cortex_m::asm::delay(5);
-    let tx = [0b100_11111, 0b11];
-    unwrap!(spim.transfer(&mut rx, &tx).await);
-    cortex_m::asm::delay(10);
-    ncs.set_high();
-
-    // read EREVID
-    cortex_m::asm::delay(10);
-    ncs.set_low();
-    cortex_m::asm::delay(5);
-    let tx = [0b000_10010, 0];
-    unwrap!(spim.transfer(&mut rx, &tx).await);
-    cortex_m::asm::delay(10);
-    ncs.set_high();
-
-    info!("erevid: {=[?]}", rx);
+    buf.fill(0);
+    spim.transfer(&mut buf, &data).await.unwrap();
+    assert_eq!(data, buf);
 }
