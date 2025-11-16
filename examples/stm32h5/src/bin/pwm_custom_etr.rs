5386db45617d5c76691b0f741377c4c34a7ed027
Albin Hedman <albin9604@gmail.com>
2025-11-16 19:41:37 +0100

    Fix h5 examples

--- examples/stm32h5/src/bin/pwm_custom_etr.rs
+++ examples/stm32h5/src/bin/pwm_custom_etr.rs
@@ -3,14 +3,11 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{AfioRemap, OutputType};
 use embassy_stm32::pac::timer::vals::{Etp, Etps};
-use embassy_stm32::timer::custom_timer::{CustomPwmBuilder, TriggerMode, TriggerSource};
-use embassy_stm32::timer::low_level::{FilterValue, InputCaptureMode, InputTISelection, OutputCompareMode};
-use embassy_stm32::timer::{Channel, UpDma};
+use embassy_stm32::timer::Channel;
+use embassy_stm32::timer::custom_timer::{CustomPwmBuilder, TriggerMode};
+use embassy_stm32::timer::low_level::FilterValue;
 
-use embassy_stm32::timer::simple_pwm::PwmPin;
-use embassy_stm32::{Peri, peripherals};
 use {defmt_rtt as _, panic_probe as _};
 
 ///            0               CCR              ARR          0               CCR
@@ -44,15 +41,12 @@ use {defmt_rtt as _, panic_probe as _};
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
-    let dma = p.DMA1_CH5;
-    let out_pin = PwmPin::new(p.PA9, OutputType::PushPull);
-    let trigger_pin = p.PA8;
-    let capture_pin = p.PA10;
+    let trigger_pin = p.PA12;
 
-    let mut tim = CustomPwmBuilder::<_, _, _, _, _, _, _>::new(tim)
+    let mut tim = CustomPwmBuilder::new(p.TIM1)
         //.frequency(Hertz(123))
         .prescaler_and_period(0, 1337)
-        .etr::<AfioRemap<0>>(trigger_pin, FilterValue::FDTS_DIV32_N8, Etp::NOT_INVERTED, Etps::DIV1)
+        .etr(trigger_pin, FilterValue::FDTS_DIV32_N8, Etp::NOT_INVERTED, Etps::DIV1)
         .trigger_from_etr(TriggerMode::TriggerMode)
         .ch1_internal(1234)
         .one_pulse_mode()
