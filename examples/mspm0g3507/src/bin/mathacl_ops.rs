5729b653ae5a95fe1af131dcbceb9dbb0397f4c6
Siarhei B <dkc.sergey.dema@gmail.com>
2025-11-16 01:16:30 +0100

    mspm0: add example of MATHACL based & tested on mspm0g3507

---
+++ examples/mspm0g3507/src/bin/mathacl_ops.rs
@@ -0,0 +1,37 @@
+//! Example of using mathematical calculations performed by the MSPM0G3507 chip.
+//!
+//! It prints the result of basics trigonometric calculation.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_mspm0::mathacl::{Mathacl, Precision};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_halt as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    info!("Hello world!");
+
+    let d = embassy_mspm0::init(Default::default());
+
+    let mut macl = Mathacl::new(d.MATHACL);
+
+    // in range [-1,1)
+    let angle = 0.5;
+    match macl.sin(angle, Precision::High) {
+        Ok(res) => info!("sin({}) = {}", angle*180.0, res),
+        Err(e) => error!("sin Error: {:?}", e),
+    }
+
+    match macl.cos(angle, Precision::Medium) {
+        Ok(res) => info!("cos({}) = {}", angle*180.0, res),
+        Err(e) => error!("cos Error: {:?}", e),
+    }
+
+    loop {
+        Timer::after_millis(500).await;
+    }
+}
