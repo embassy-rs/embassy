da96ef9d51730975b52fa2c92ec27236b68f1daf
Ulf Lilleengen <ulf@digili.no>
2025-11-26 14:42:46 +0100

    chore: cleanup

--- embassy-nrf/src/time_driver.rs
+++ embassy-nrf/src/time_driver.rs
@@ -182,15 +182,10 @@ impl RtcDriver {
         while r.counter().read().0 != 0 {}
 
         #[cfg(feature = "_grtc")]
-        {
-            // According to datasheet, we need to wait for STATUS.LFTIMER.READY
-            // before the timer is actually started
-            loop {
-                if r.status().lftimer().read().ready() {
-                    break;
-                }
+        loop {
+            if r.status().lftimer().read().ready() {
+                break;
             }
-            // Keep SYSCOUNTER[0] always active so we can read it anytime without BUSY waits
         }
 
         #[cfg(feature = "_grtc")]
