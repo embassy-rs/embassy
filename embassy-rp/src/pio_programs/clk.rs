fd40190823583435ef286992688e8dbeb5ae9e71
Łukasz Dobrowolski <lukasz@dobrowolski.io>
2025-11-27 22:51:53 +0100

    embassy-rp: Add a simple clk generator using pio

---
+++ embassy-rp/src/pio_programs/clk.rs
@@ -0,0 +1,66 @@
+//! Clock generator using PIO
+
+use crate::Peri;
+use crate::pio::program::pio_asm;
+use crate::pio::{Common, Config, Direction, Instance, LoadedProgram, Pin, PioPin, StateMachine};
+use crate::pio_programs::clock_divider::calculate_pio_clock_divider;
+
+/// Clock PIO program.
+pub struct PioClkProgram<'a, PIO: Instance> {
+    prg: LoadedProgram<'a, PIO>,
+}
+
+impl<'a, PIO: Instance> PioClkProgram<'a, PIO> {
+    /// Load the program into PIO instruction memory.
+    pub fn new(common: &mut Common<'a, PIO>) -> Self {
+        let prg = pio_asm!("set pins 0", "set pins 1");
+        let prg = common.load_program(&prg.program);
+        Self { prg }
+    }
+}
+
+/// Pio backed PWM output
+pub struct PioClk<'d, T: Instance, const SM: usize> {
+    sm: StateMachine<'d, T, SM>,
+    pin: Pin<'d, T>,
+}
+
+impl<'d, T: Instance, const SM: usize> PioClk<'d, T, SM> {
+    /// Configure state mashine for clock generation.
+    pub fn new(
+        pio: &mut Common<'d, T>,
+        mut sm: StateMachine<'d, T, SM>,
+        pin: Peri<'d, impl PioPin>,
+        program: &PioClkProgram<'d, T>,
+        frequency: u32,
+    ) -> Self {
+        let pin = pio.make_pio_pin(pin);
+        sm.set_pin_dirs(Direction::Out, &[&pin]);
+
+        let mut cfg = Config::default();
+        let program_critical_loop_len = 2;
+        let sm_frequency = frequency * program_critical_loop_len;
+        cfg.clock_divider = calculate_pio_clock_divider(sm_frequency);
+        cfg.set_set_pins(&[&pin]);
+        cfg.use_program(&program.prg, &[]);
+
+        sm.set_config(&cfg);
+
+        Self { sm, pin }
+    }
+
+    /// Start emmiting clock.
+    pub fn start(&mut self) {
+        self.sm.set_enable(true);
+    }
+
+    /// Stop emmiting clock.
+    pub fn stop(&mut self) {
+        self.sm.set_enable(false);
+    }
+
+    /// Return the state machine and pin.
+    pub fn release(self) -> (StateMachine<'d, T, SM>, Pin<'d, T>) {
+        (self.sm, self.pin)
+    }
+}
