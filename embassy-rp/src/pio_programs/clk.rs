f9d6bf8167cb98da99311a0743985d638b2e4257
Łukasz Dobrowolski <lukasz@dobrowolski.io>
2025-12-02 21:47:59 +0100

    Improve docs

--- embassy-rp/src/pio_programs/clk.rs
+++ embassy-rp/src/pio_programs/clk.rs
@@ -5,13 +5,13 @@ use crate::pio::program::pio_asm;
 use crate::pio::{Common, Config, Direction, Instance, LoadedProgram, Pin, PioPin, StateMachine};
 use crate::pio_programs::clock_divider::calculate_pio_clock_divider;
 
-/// Clock PIO program.
+/// Clock generator PIO program
 pub struct PioClkProgram<'a, PIO: Instance> {
     prg: LoadedProgram<'a, PIO>,
 }
 
 impl<'a, PIO: Instance> PioClkProgram<'a, PIO> {
-    /// Load the program into PIO instruction memory.
+    /// Load the program into PIO instruction memory
     pub fn new(common: &mut Common<'a, PIO>) -> Self {
         let prg = pio_asm!("set pins 0", "set pins 1");
         let prg = common.load_program(&prg.program);
@@ -19,14 +19,14 @@ impl<'a, PIO: Instance> PioClkProgram<'a, PIO> {
     }
 }
 
-/// Pio backed PWM output
+/// Pio backed clock generator
 pub struct PioClk<'d, T: Instance, const SM: usize> {
     sm: StateMachine<'d, T, SM>,
     pin: Pin<'d, T>,
 }
 
 impl<'d, T: Instance, const SM: usize> PioClk<'d, T, SM> {
-    /// Configure state mashine for clock generation.
+    /// Configure state mashine for clock generation
     pub fn new(
         pio: &mut Common<'d, T>,
         mut sm: StateMachine<'d, T, SM>,
@@ -49,17 +49,17 @@ impl<'d, T: Instance, const SM: usize> PioClk<'d, T, SM> {
         Self { sm, pin }
     }
 
-    /// Start emmiting clock.
+    /// Start emmiting clock
     pub fn start(&mut self) {
         self.sm.set_enable(true);
     }
 
-    /// Stop emmiting clock.
+    /// Stop emmiting clock
     pub fn stop(&mut self) {
         self.sm.set_enable(false);
     }
 
-    /// Return the state machine and pin.
+    /// Return the state machine and pin
     pub fn release(self) -> (StateMachine<'d, T, SM>, Pin<'d, T>) {
         (self.sm, self.pin)
     }
