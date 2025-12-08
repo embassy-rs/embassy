2a54a3b93f38569a0a7aa585e75d6939a1726cce
Ulf Lilleengen <ulf@digili.no>
2025-12-08 11:27:07 +0100

    chore: update devtool dependency

--- .github/ci/janitor.sh
+++ .github/ci/janitor.sh
@@ -9,7 +9,7 @@ export CARGO_HOME=/ci/cache/cargo
 export CARGO_TARGET_DIR=/ci/cache/target
 export PATH=$CARGO_HOME/bin:$PATH
 
-cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev d015fd5e972a3e550ebef0da6748099b88a93ba6
+cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev c60400e213f7eb0296581183140ec147dd7a848b
 
 cargo embassy-devtool check-crlf
 cargo embassy-devtool check-manifest
