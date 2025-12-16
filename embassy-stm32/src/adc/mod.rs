b9395180fba64d42fb3e0977f4882d7555488c2f
Gabriel Smith <ga29smith@gmail.com>
2025-12-16 12:27:43 -0500

    stm32/adc: allow DMA reads to loop through enabled channels
    This was previously implemented for v3 ADCs, but that was lost in the
    recent refactoring. I have gone through all the reference manuals to
    verify that this should be supported for all chips, but it has only
    been confirmed for the STM32H5 series.

--- embassy-stm32/src/adc/mod.rs
+++ embassy-stm32/src/adc/mod.rs
@@ -214,7 +214,7 @@ impl<'d, T: Instance> Adc<'d, T> {
     #[cfg(any(adc_g4, adc_v3, adc_g0, adc_h5, adc_h7rs, adc_u0, adc_v4, adc_u5, adc_wba, adc_c0))]
     /// Read one or multiple ADC regular channels using DMA.
     ///
-    /// `sequence` iterator and `readings` must have the same length.
+    /// `readings` must have a length that is a multiple of the length of the `sequence` iterator.
     ///
     /// Example
     /// ```rust,ignore
@@ -253,8 +253,8 @@ impl<'d, T: Instance> Adc<'d, T> {
     ) {
         assert!(sequence.len() != 0, "Asynchronous read sequence cannot be empty");
         assert!(
-            sequence.len() == readings.len(),
-            "Sequence length must be equal to readings length"
+            readings.len() % sequence.len() == 0,
+            "Readings length must be a multiple of sequence length"
         );
         assert!(
             sequence.len() <= 16,
