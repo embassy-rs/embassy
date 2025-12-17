20bf3b41b24ca29280871e504dc819873c8813c3
Cesar Tamayo Claro <cesardtamayo@gmail.com>
2025-12-17 13:12:09 -0700

    Undo stm32f3 temp changes

--- examples/stm32f3/src/bin/blinky.rs
+++ examples/stm32f3/src/bin/blinky.rs
@@ -12,7 +12,7 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let mut led = Output::new(p.PB13, Level::High, Speed::Low);
+    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
 
     loop {
         info!("high");
