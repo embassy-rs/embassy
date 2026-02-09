d707fe3b48022219d4ce9c2467b3b27538ada900
Felipe Balbi <febalbi@microsoft.com>
2026-02-09 08:51:33 -0800

    [MCXA] ctimer: capture: simplify example
    Use a gpio instead of a PWM.

--- examples/mcxa/src/bin/capture.rs
+++ examples/mcxa/src/bin/capture.rs
@@ -2,12 +2,13 @@
 #![no_main]
 
 use embassy_executor::Spawner;
+use embassy_time::Timer;
 use hal::bind_interrupts;
 use hal::clocks::config::Div8;
 use hal::config::Config;
 use hal::ctimer::CTimer;
 use hal::ctimer::capture::{self, Capture, Edge, InterruptHandler};
-use hal::ctimer::pwm::{SetDutyCycle, SinglePwm};
+use hal::gpio::{DriveStrength, Level, Output, SlewRate};
 use hal::peripherals::CTIMER2;
 use {defmt_rtt as _, embassy_mcxa as hal, panic_probe as _};
 
@@ -16,23 +17,22 @@ bind_interrupts!(struct Irqs {
 });
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let mut config = Config::default();
     config.clock_cfg.sirc.fro_lf_div = Div8::from_divisor(1);
 
     let p = hal::init(config);
 
-    defmt::info!("Pwm example");
+    defmt::info!("Capture example");
 
-    let pwm_ctimer = CTimer::new(p.CTIMER1, Default::default()).unwrap();
-    let mut pwm = SinglePwm::new(pwm_ctimer, p.CTIMER1_CH2, p.CTIMER1_CH0, p.P3_12, Default::default()).unwrap();
+    let pin = Output::new(p.P3_12, Level::High, DriveStrength::Normal, SlewRate::Fast);
 
-    let cap_ctimer = CTimer::new(p.CTIMER2, Default::default()).unwrap();
+    let ctimer = CTimer::new(p.CTIMER2, Default::default()).unwrap();
     let mut config = capture::Config::default();
     config.edge = Edge::RisingEdge;
-    let mut capture = Capture::new_with_input_pin(cap_ctimer, p.CTIMER2_CH0, p.P3_14, Irqs, config).unwrap();
+    let mut capture = Capture::new_with_input_pin(ctimer, p.CTIMER2_CH0, p.P3_14, Irqs, config).unwrap();
 
-    pwm.set_duty_cycle_percent(50).unwrap();
+    spawner.spawn(pin_task(pin).unwrap());
 
     loop {
         let one = capture.capture().await.unwrap();
@@ -43,5 +43,16 @@ async fn main(_spawner: Spawner) {
             diff.to_period(capture.frequency()),
             diff.to_frequency(capture.frequency())
         );
+    }
+}
+
+#[embassy_executor::task]
+async fn pin_task(mut pin: Output<'static>) {
+    Timer::after_secs(1).await;
+
+    loop {
+        pin.set_high();
+        pin.set_low();
+        Timer::after_secs(1).await;
     }
 }
