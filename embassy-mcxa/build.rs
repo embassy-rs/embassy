52d45b5dab93973051c9074441091d01f9b7d86b
Felipe Balbi <febalbi@microsoft.com>
2026-02-18 13:10:39 -0800

    [MCXA] Also generate memory.x

--- embassy-mcxa/build.rs
+++ embassy-mcxa/build.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fmt::Write as _;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let chip_name = match env::vars()
@@ -15,6 +17,37 @@ fn main() {
     .to_ascii_lowercase();
 
     eprintln!("chip: {chip_name}");
+
+    println!("cargo:rerun-if-changed=build.rs");
+
+    if cfg!(feature = "memory-x") {
+        let out_dir = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+        gen_memory_x(out_dir);
+        println!("cargo:rustc-link-search={}", out_dir.display());
+    }
+}
+
+fn gen_memory_x(out_dir: &Path) {
+    let mut memory_x = String::new();
+
+    let flash = if cfg!(feature = "_flash-512k") {
+        512
+    } else /* if cfg!(feature = "_flash-1024k") */ {
+        1024
+    };
+
+    let ram = if cfg!(feature = "_sram-128k") {
+        128
+    } else /* if cfg!(feature = "_sram-256k") */ {
+        256
+    };
+
+    write!(memory_x, "MEMORY\n{{\n").unwrap();
+    writeln!(memory_x, "    FLASH : ORIGIN = 0x00000000, LENGTH = {:>4}K", flash,).unwrap();
+    writeln!(memory_x, "    RAM   : ORIGIN = 0x20000000, LENGTH = {:>4}K", ram).unwrap();
+    write!(memory_x, "}}").unwrap();
+
+    std::fs::write(out_dir.join("memory.x"), memory_x.as_bytes()).unwrap();
 }
 
 enum GetOneError {
