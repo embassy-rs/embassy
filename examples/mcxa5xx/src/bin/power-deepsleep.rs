c5e9b0bc24bf5489f3cacaedc9af925420d5935b
James Munns <james@onevariable.com>
2026-03-11 17:43:35 +0100

    wip

--- examples/mcxa5xx/src/bin/power-deepsleep.rs
+++ examples/mcxa5xx/src/bin/power-deepsleep.rs
@@ -1,6 +1,6 @@
-//! This example roughly emulates the `IDD_SLEEP_MD_3` scenario from the datasheet.
+//! This example roughly emulates the `IDD_DEEP_SLEEP_MD_2` scenario from the datasheet.
 //!
-//! As written, this achieves 579uA average current when measured with a Nordic PPK2.
+//! As written, this achieves 153uA average current when measured with a Nordic PPK2.
 //!
 //! **NOTE: This requires rework of the board! You must remove R26 (used for the on
 //! board op-amp), remove R52, and bodge the pad of R52 that is closest to R61 to TP9
@@ -21,23 +21,28 @@ use embassy_time::Timer;
 use hal::gpio::{DriveStrength, Level, Output, SlewRate};
 use {defmt_rtt as _, embassy_mcxa as hal, panic_probe as _};
 
-#[embassy_executor::main]
+#[cfg_attr(
+    feature = "custom-executor",
+    embassy_executor::main(executor = "embassy_mcxa::executor::Executor", entry = "cortex_m_rt::entry")
+)]
+#[cfg_attr(not(feature = "custom-executor"), embassy_executor::main)]
 async fn main(_spawner: Spawner) {
     // Do a short delay in order to allow for us to attach the debugger/start
     // a flash in case some setting below is wrong, and the CPU gets stuck
     // in deep sleep with debugging disabled.
     defmt::info!("Pre-power delay!");
     // Experimentally: about 5-6s or so.
-    cortex_m::asm::delay(45_000_000 * 2);
+    cortex_m::asm::delay(48_000_000);
     defmt::info!("Pre-power delay complete!");
     let mut cfg = hal::config::Config::default();
 
-    // Disable 45M osc
+    // Disable 48M osc
     cfg.clock_cfg.firc = None;
 
     // Enable 12M osc to use as core clock
     cfg.clock_cfg.sirc.fro_12m_enabled = true;
-    cfg.clock_cfg.sirc.fro_lf_div = None;
+    cfg.clock_cfg.sirc.fro_lf_div = Some(Div8::no_div());
+    cfg.clock_cfg.sirc.power = PoweredClock::AlwaysEnabled;
 
     // Disable 16K osc
     cfg.clock_cfg.fro16k = None;
@@ -51,7 +56,7 @@ async fn main(_spawner: Spawner) {
     // Feed core from 12M osc
     cfg.clock_cfg.main_clock = MainClockConfig {
         source: MainClockSource::SircFro12M,
-        power: PoweredClock::NormalEnabledDeepSleepDisabled,
+        power: PoweredClock::AlwaysEnabled,
         ahb_clk_div: Div8::no_div(),
     };
 
@@ -61,8 +66,8 @@ async fn main(_spawner: Spawner) {
     cfg.clock_cfg.vdd_power.active_mode.drive = VddDriveStrength::Low { enable_bandgap: false };
     cfg.clock_cfg.vdd_power.low_power_mode.drive = VddDriveStrength::Low { enable_bandgap: false };
 
-    // Set "gated" core mode, allowing clock to the core to be gated on sleep
-    cfg.clock_cfg.vdd_power.core_sleep = CoreSleep::WfeGated;
+    // Set "deep sleep" mode
+    cfg.clock_cfg.vdd_power.core_sleep = CoreSleep::DeepSleep;
 
     // Set flash doze, allowing internal flash clocks to be gated on sleep
     cfg.clock_cfg.vdd_power.flash_sleep = FlashSleep::FlashDoze;
@@ -70,7 +75,7 @@ async fn main(_spawner: Spawner) {
     let p = hal::init(cfg);
 
     defmt::info!("Going to sleep shortly...");
-    cortex_m::asm::delay(45_000_000 / 2);
+    cortex_m::asm::delay(48_000_000 / 4);
 
     let mut red = Output::new(p.P3_18, Level::High, DriveStrength::Normal, SlewRate::Slow);
     loop {
