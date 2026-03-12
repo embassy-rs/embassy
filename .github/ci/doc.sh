a21cd1fb87bf6a8429ece03cc878a33f68c75663
xoviat <xoviat@users.noreply.github.com>
2026-03-12 08:13:25 -0500

    ci: disable docs build

--- .github/ci/doc.sh
+++ .github/ci/doc.sh
@@ -15,9 +15,9 @@ mv rust-toolchain-nightly.toml rust-toolchain.toml
 cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev 8f4cfa11324c582467c2aab161ef963ff7a2b884
 cargo install --git https://github.com/embassy-rs/docserver --locked --rev 09805a79beef037d283192146e2b546cb1c0e931
 
-cargo embassy-devtool doc -o webroot
+# cargo embassy-devtool doc -o webroot
 
-export KUBECONFIG=/ci/secrets/kubeconfig.yml
-POD=$(kubectl get po -l app=docserver -o jsonpath={.items[0].metadata.name})
-kubectl cp webroot/crates $POD:/data
-kubectl cp webroot/static $POD:/data
+# export KUBECONFIG=/ci/secrets/kubeconfig.yml
+# POD=$(kubectl get po -l app=docserver -o jsonpath={.items[0].metadata.name})
+# kubectl cp webroot/crates $POD:/data
+# kubectl cp webroot/static $POD:/data
