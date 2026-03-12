49fa75c6c5e932d83a3dac4ee8349822ad48cc13
Ulf Lilleengen <ulf@digili.no>
2026-03-12 12:30:59 +0100

    chore: rustfmt

--- embassy-executor/build.rs
+++ embassy-executor/build.rs
@@ -9,7 +9,9 @@ fn main() {
     #[cfg(feature = "executor-thread")]
     println!("cargo:warning=The `executor-thread` feature is deprecated. Use `executor-thread-single-core` instead.");
     #[cfg(feature = "executor-interrupt")]
-    println!("cargo:warning=The `executor-interrupt` feature is deprecated. Use `executor-interrupt-single-core` instead.");
+    println!(
+        "cargo:warning=The `executor-interrupt` feature is deprecated. Use `executor-interrupt-single-core` instead."
+    );
 
     // This is used to exclude legacy architecture support. The raw executor needs to be used for
     // those architectures because SEV/WFE are not supported.
