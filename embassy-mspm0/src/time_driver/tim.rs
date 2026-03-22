cb5d8f4269fa4043c0c67f47011e90b98bae3de5
ferris <ferris@devdroplets.com>
2026-03-22 21:23:38 +0100

    Fix feature matrix to match pac (not trm)

--- embassy-mspm0/src/time_driver/tim.rs
+++ embassy-mspm0/src/time_driver/tim.rs
@@ -7,7 +7,6 @@ use embassy_hal_internal::interrupt::InterruptExt;
 use embassy_time_driver::Driver;
 use embassy_time_queue_utils::Queue;
 use mspm0_metapac::interrupt;
-use mspm0_metapac::tim::Tim;
 use mspm0_metapac::tim::vals::{Cm, Cvae, CxC, EvtCfg, PwrenKey, Repeat, ResetKey};
 
 use crate::interrupt::typelevel::Interrupt;
