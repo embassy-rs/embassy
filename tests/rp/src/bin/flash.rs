6369f88c7870c5d71a17269c541d4135d9cbcfb8
Nick Dowsett <nickdowsett42@gmail.com>
2026-03-24 23:05:45 +0800

    examples: remove delay in rp flash examples

--- tests/rp/src/bin/flash.rs
+++ tests/rp/src/bin/flash.rs
@@ -10,7 +10,6 @@ use embassy_executor::Spawner;
 use embassy_rp::flash::{Async, ERASE_SIZE, FLASH_BASE};
 use embassy_rp::peripherals::DMA_CH0;
 use embassy_rp::{bind_interrupts, dma};
-use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
@@ -23,12 +22,6 @@ const ADDR_OFFSET: u32 = 0x8000;
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     info!("Hello World!");
-
-    // add some delay to give an attached debug probe time to parse the
-    // defmt RTT header. Reading that header might touch flash memory, which
-    // interferes with flash write operations.
-    // https://github.com/knurling-rs/defmt/pull/683
-    Timer::after_millis(10).await;
 
     let mut flash = embassy_rp::flash::Flash::<_, Async, { 2 * 1024 * 1024 }>::new(p.FLASH, p.DMA_CH0, Irqs);
 
