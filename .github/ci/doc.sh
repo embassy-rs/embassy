8b6138737a1e4e2a831192311ac0c830f9967cd3
xoviat <xoviat@users.noreply.github.com>
2026-04-05 17:02:48 -0500

    ci: update docserver

--- .github/ci/doc.sh
+++ .github/ci/doc.sh
@@ -13,7 +13,7 @@ export PATH=$CARGO_HOME/bin:$PATH
 mv rust-toolchain-nightly.toml rust-toolchain.toml
 
 cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev 8f4cfa11324c582467c2aab161ef963ff7a2b884
-cargo install --git https://github.com/embassy-rs/docserver --locked --rev 09bd35de8ee1ca7ab71adb7b551407bbbed6a1c0
+cargo install --git https://github.com/embassy-rs/docserver --locked --rev e16c30dcc60a41641fd73bd4ad1a8c4bd57d792d
 
 cargo embassy-devtool doc -o webroot
 
