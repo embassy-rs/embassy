21e895adb4972eb8fabc8aca44bc3c06240a9f9d
KingCol13 <48412633+KingCol13@users.noreply.github.com>
2026-04-25 19:33:35 +0100

    Use dummy DMA rather than IRQ for PIO QSPI sync

--- examples/rp235x/src/bin/ethernet_w6300_qspi_udp.rs
+++ examples/rp235x/src/bin/ethernet_w6300_qspi_udp.rs
@@ -57,7 +57,7 @@ async fn main(spawner: Spawner) {
     spi_cfg.frequency = 25_000_000;
 
     let Pio {
-        mut common, sm0, irq0, ..
+        mut common, sm0, ..
     } = Pio::new(rp_peripherals.PIO0, Irqs);
 
     let clk = rp_peripherals.PIN_17;
@@ -69,7 +69,6 @@ async fn main(spawner: Spawner) {
     let qspi = Qspi::new(
         &mut common,
         sm0,
-        irq0,
         clk,
         qspi_0,
         qspi_1,
