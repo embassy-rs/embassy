b9defa80964e44a4475a5e4b0cc27e9dda5169bf
Albin Hedman <albin9604@gmail.com>
2026-05-07 11:37:12 +0200

    Update examples for new backup sram API

--- examples/stm32h5/src/bin/backup_sram.rs
+++ examples/stm32h5/src/bin/backup_sram.rs
@@ -2,11 +2,12 @@
 #![no_main]
 
 use defmt::*;
+use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::Config;
 use embassy_stm32::backup_sram::BackupMemory;
 use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -16,16 +17,17 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(config);
     info!("Started!");
 
-    let (bytes, status) = BackupMemory::new(p.BKPSRAM);
+    let mut backup_ram = BackupMemory::new(p.BKPSRAM);
 
-    match status {
+    match backup_ram.is_retained() {
         false => info!("BKPSRAM just enabled"),
         true => info!("BKPSRAM already enabled"),
     }
 
     loop {
-        info!("byte0: {}", bytes[0]);
-        bytes[0] = bytes[0].wrapping_add(1);
+        let byte0 = backup_ram.get(0);
+        info!("byte0: {}", byte0);
+        backup_ram.set(0, byte0.wrapping_add(1));
         Timer::after_millis(500).await;
     }
 }
