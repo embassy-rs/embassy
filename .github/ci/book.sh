59d323a282257d6e7e1cedbc7e6b4932027a63f8
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2026-05-16 01:13:38 +0200

    asfd

--- .github/ci/book.sh
+++ .github/ci/book.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-## on push branch=main
-## priority -100
-## dedup dequeue
-## cooldown 15m
+## on push branch~=gh-readonly-queue/main/.*
+## on pull_request
 
 set -euxo pipefail
 
 make -C docs
-
-export KUBECONFIG=/ci/secrets/kubeconfig.yml
-POD=$(kubectl get po -l app=website -o jsonpath={.items[0].metadata.name})
-
-mkdir -p build
-mv docs/book build/book
-tar -C build -cf book.tar book
-kubectl exec $POD -- mkdir -p /usr/share/nginx/html
-kubectl cp book.tar $POD:/usr/share/nginx/html/
-kubectl exec $POD -- find /usr/share/nginx/html
-kubectl exec $POD -- tar -C /usr/share/nginx/html -xvf /usr/share/nginx/html/book.tar
