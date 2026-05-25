20e90073561ac253b9f964a4525531aed776975c
xoviat <xoviat@users.noreply.github.com>
2026-05-25 16:31:16 -0500

    stm32: use atomic u32

--- embassy-stm32/src/reg.rs
+++ embassy-stm32/src/reg.rs
@@ -15,51 +15,63 @@ pub trait AtomicModify<T: Sized> {
 impl<T: Copy, A: Read + Write> AtomicModify<T> for Reg<T, A> {
     fn set_bits(&self, f: impl FnOnce(&mut T)) {
         unsafe {
-            #[cfg(target_has_atomic = "ptr")]
-            use core::sync::atomic::{AtomicUsize, Ordering};
+            #[cfg(target_has_atomic = "32")]
+            use core::sync::atomic::{AtomicU32, Ordering, compiler_fence};
 
-            let mut v = usize::MIN;
+            let mut v = u32::MIN;
 
-            core::assert_eq!(size_of::<usize>(), size_of::<T>());
-            core::assert_eq!(align_of::<usize>(), align_of::<T>());
+            core::assert_eq!(size_of::<u32>(), size_of::<T>());
+            core::assert_eq!(align_of::<u32>(), align_of::<T>());
 
             f(&mut *(&raw mut v as *mut T));
 
-            let ptr = self.as_ptr() as *mut usize;
+            let ptr = self.as_ptr() as *mut u32;
 
-            #[cfg(not(target_has_atomic = "ptr"))]
+            #[cfg(not(target_has_atomic = "32"))]
             critical_section::with(|_| {
                 let val = ptr.read_volatile();
                 ptr.write_volatile(val | v);
             });
 
-            #[cfg(target_has_atomic = "ptr")]
-            AtomicUsize::from_ptr(ptr).fetch_or(v, Ordering::Relaxed);
+            #[cfg(target_has_atomic = "32")]
+            compiler_fence(Ordering::Release);
+
+            #[cfg(target_has_atomic = "32")]
+            AtomicU32::from_ptr(ptr).fetch_or(v, Ordering::Relaxed);
+
+            #[cfg(target_has_atomic = "32")]
+            compiler_fence(Ordering::Release);
         }
     }
 
     fn clear_bits(&self, f: impl FnOnce(&mut T)) {
         unsafe {
-            #[cfg(target_has_atomic = "ptr")]
-            use core::sync::atomic::{AtomicUsize, Ordering};
+            #[cfg(target_has_atomic = "32")]
+            use core::sync::atomic::{AtomicU32, Ordering, compiler_fence};
 
-            let mut v = usize::MAX;
+            let mut v = u32::MAX;
 
-            core::assert_eq!(size_of::<usize>(), size_of::<T>());
-            core::assert_eq!(align_of::<usize>(), align_of::<T>());
+            core::assert_eq!(size_of::<u32>(), size_of::<T>());
+            core::assert_eq!(align_of::<u32>(), align_of::<T>());
 
             f(&mut *(&raw mut v as *mut T));
 
-            let ptr = self.as_ptr() as *mut usize;
+            let ptr = self.as_ptr() as *mut u32;
 
-            #[cfg(not(target_has_atomic = "ptr"))]
+            #[cfg(not(target_has_atomic = "32"))]
             critical_section::with(|_| {
                 let val = ptr.read_volatile();
                 ptr.write_volatile(val & v);
             });
 
-            #[cfg(target_has_atomic = "ptr")]
-            AtomicUsize::from_ptr(ptr).fetch_and(v, Ordering::Relaxed);
+            #[cfg(target_has_atomic = "32")]
+            compiler_fence(Ordering::Release);
+
+            #[cfg(target_has_atomic = "32")]
+            AtomicU32::from_ptr(ptr).fetch_and(v, Ordering::Relaxed);
+
+            #[cfg(target_has_atomic = "32")]
+            compiler_fence(Ordering::Release);
         }
     }
 }
