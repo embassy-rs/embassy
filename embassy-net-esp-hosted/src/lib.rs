4df3c991f96661e7600cf35861c0bb32ee8499cc
Dániel Buga <bugadani@gmail.com>
2026-06-06 09:59:03 +0200

    Move fg proto into nested folder

--- embassy-net-esp-hosted/src/lib.rs
+++ embassy-net-esp-hosted/src/lib.rs
@@ -12,12 +12,6 @@ use embedded_hal::digital::OutputPin;
 use crate::ioctl::{PendingIoctl, Shared};
 use crate::proto::{CtrlMsg, CtrlMsg_};
 
-#[allow(unused)]
-#[allow(non_snake_case)]
-#[allow(non_camel_case_types)]
-#[allow(non_upper_case_globals)]
-#[allow(missing_docs)]
-#[allow(clippy::all)]
 mod proto;
 
 // must be first
---
+++ embassy-net-esp-hosted/src/proto/mod.rs
@@ -0,0 +1,11 @@
+//! ESP-Hosted protobuf message bindings.
+
+#[allow(unused)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(missing_docs)]
+#[allow(clippy::all)]
+mod fg;
+
+pub use fg::*;
