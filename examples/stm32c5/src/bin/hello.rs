fadf78ac4a3d4591922b9cdb3c93926bf959ddce
xoviat <xoviat@users.noreply.github.com>
2026-06-09 14:47:40 -0500

    c5: fix example

--- examples/stm32c5/src/bin/hello.rs
+++ examples/stm32c5/src/bin/hello.rs
@@ -1,14 +1,15 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
-// Some panic handler needs to be included. This one halts the processor on panic.
-use cortex_m_rt::entry;
-use {defmt_rtt as _, panic_probe as _};
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use panic_probe as _;
 
-// Use `main` as the entry point of this application, which may not return.
-#[entry]
-fn main() -> ! {
-    defmt::println!("Hello world! I am an STM32C5");
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let _p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
 
     loop {}
 }
