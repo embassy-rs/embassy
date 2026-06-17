c6a3df006bcdff44d605baf7ed3eedb0e97b34f4
vic1707 <28602203+vic1707@users.noreply.github.com>
2026-06-17 13:23:14 +0200

    more defmt derives

--- embassy-rp/src/lib.rs
+++ embassy-rp/src/lib.rs
@@ -597,6 +597,7 @@ pub mod config {
 
     /// HAL configuration passed when initializing.
     #[non_exhaustive]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
     pub struct Config {
         /// Clock configuration.
         pub clocks: ClockConfig,
