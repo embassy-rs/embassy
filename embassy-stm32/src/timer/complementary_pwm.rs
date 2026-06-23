508d6020ae3814897d6a9e713b175f5c049935dc
Pufferfish <puffish@puffish.net>
2026-06-23 18:57:46 +0200

    use binary representation

--- embassy-stm32/src/timer/complementary_pwm.rs
+++ embassy-stm32/src/timer/complementary_pwm.rs
@@ -820,15 +820,15 @@ fn compute_dead_time_value(value: u16) -> (Ckd, u8) {
             0..127 | 127 => (target as u8, target),
             128..254 | 254..256 => {
                 let tmp = div_round(value, outdiv * 2);
-                ((tmp as u8 - 64) | 128, tmp * 2)
+                ((tmp as u8 - 64) | 0b100_00000, tmp * 2)
             }
             256..504 | 504..508 => {
                 let tmp = div_round(value, outdiv * 8);
-                ((tmp as u8 - 32) | 192, tmp * 8)
+                ((tmp as u8 - 32) | 0b110_00000, tmp * 8)
             }
             508..512 | 512..1008 => {
                 let tmp = div_round(value, outdiv * 16);
-                ((tmp as u8 - 32) | 224, tmp * 16)
+                ((tmp as u8 - 32) | 0b111_00000, tmp * 16)
             }
             1008.. => (u8::MAX, 1008),
         };
