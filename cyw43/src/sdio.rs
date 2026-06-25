c2aed2494c342837df3ea8dfe7dcbd3fd039eb18
xoviat <xoviat@users.noreply.github.com>
2026-06-24 20:46:27 -0500

    remove aligned from embassy-hal-internal

--- cyw43/src/sdio.rs
+++ cyw43/src/sdio.rs
@@ -1,5 +1,6 @@
-use aligned::{A4, Aligned};
-use embassy_hal_internal::aligned::{ToAligned, ToMutAligned};
+use core::mem;
+
+use aligned::{A4, Aligned, Alignment};
 use embassy_time::{Delay, Duration, Timer};
 
 use crate::WithContext;
@@ -18,6 +19,46 @@ use crate::util::try_until;
 //         ALIGN_UINT!($len, 64)
 //     };
 // }
+
+/// Create an aligned value from a non-aligned value
+pub trait ToAligned {
+    /// Element
+    type Element: ?Sized;
+
+    /// Create a type-checked aligned value from a value that is aligned.
+    fn to_aligned<A: Alignment>(&self) -> &Aligned<A, Self::Element>;
+}
+
+impl<T: ?Sized> ToAligned for &T {
+    type Element = T;
+
+    #[inline]
+    fn to_aligned<A: Alignment>(&self) -> &Aligned<A, Self::Element> {
+        assert!(self as *const _ as usize % A::ALIGN == 0);
+
+        unsafe { mem::transmute(*self) }
+    }
+}
+
+/// Create an aligned value from a non-aligned value
+pub trait ToMutAligned {
+    /// Element
+    type Element: ?Sized;
+
+    /// Create a type-checked aligned value from a value that is aligned.
+    fn to_mut_aligned<A: Alignment>(&mut self) -> &mut Aligned<A, Self::Element>;
+}
+
+impl<T: ?Sized> ToMutAligned for T {
+    type Element = T;
+
+    #[inline]
+    fn to_mut_aligned<A: Alignment>(&mut self) -> &mut Aligned<A, Self::Element> {
+        assert!(self as *mut _ as *mut u8 as usize % A::ALIGN == 0);
+
+        unsafe { mem::transmute(&mut *self) }
+    }
+}
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
