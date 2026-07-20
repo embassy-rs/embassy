a4e35e3c3311064a046c1acf570031b11ec0843b
ferris <ferris@devdroplets.com>
2026-07-20 09:05:23 +0200

    Clean unused imports

--- embassy-rp/src/pio_programs/uart.rs
+++ embassy-rp/src/pio_programs/uart.rs
@@ -3,10 +3,8 @@
 use core::convert::Infallible;
 
 use embedded_io_async::{ErrorType, Read, Write};
-use fixed::traits::ToFixed;
 
 use crate::Peri;
-use crate::clocks::clk_sys_freq;
 use crate::gpio::Level;
 use crate::pio::{
     Common, Config, Direction as PioDirection, FifoJoin, Instance, LoadedProgram, PioPin, ShiftDirection, StateMachine,
