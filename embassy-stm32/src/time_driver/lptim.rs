a1c88b6f9f7ac66de196c94c83dc39487f89d8f8
Gerzain Mata <leftger@gmail.com>
2026-07-20 22:04:40 -0700

    stm32: merge adjacent lptim use statements (fmt)

--- embassy-stm32/src/time_driver/lptim.rs
+++ embassy-stm32/src/time_driver/lptim.rs
@@ -16,8 +16,7 @@ use stm32_metapac::lptim::Lptim;
 
 use super::AlarmState;
 use crate::interrupt::typelevel::Interrupt;
-use crate::lptim::SealedInstance;
-use crate::lptim::vals;
+use crate::lptim::{SealedInstance, vals};
 use crate::rcc::SealedRccPeripheral;
 use crate::{peripherals, rcc};
 
