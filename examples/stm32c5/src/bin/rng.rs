a0acbcd278ca4b383073b524d9f434b1c74db20e
Albin Hedman <albin9604@gmail.com>
2026-07-29 05:24:27 +0200

    C5: Add RNG example

---
+++ examples/stm32c5/src/bin/rng.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::rng::Rng;
+use embassy_stm32::{Config, bind_interrupts, peripherals, rng};
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    RNG => rng::InterruptHandler<peripherals::RNG>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = Config::default();
+    config.rcc.mux.clk48sel = embassy_stm32::rcc::mux::Clksel::HsiDiv3;
+    let p = embassy_stm32::init(config);
+    info!("Hello World!");
+
+    let mut rng = Rng::new(p.RNG, Irqs);
+
+    let mut buf = [0u8; 16];
+    unwrap!(rng.async_fill_bytes(&mut buf).await);
+    info!("random bytes: {:02x}", buf);
+}
