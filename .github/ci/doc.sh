dcf14a318eb84850391feadf76af4743effeb39b
xoviat <xoviat@users.noreply.github.com>
2026-07-30 18:22:39 -0500

    ci: use stable to build tools

--- .github/ci/doc.sh
+++ .github/ci/doc.sh
@@ -2,7 +2,7 @@
 ## on push branch=main
 ## priority -100
 ## dedup dequeue
-## cooldown 15m
+## cooldown 3m
 
 set -euxo pipefail
 
@@ -10,10 +10,11 @@ export RUSTUP_HOME=/ci/cache/rustup
 export CARGO_HOME=/ci/cache/cargo
 export CARGO_TARGET_DIR=/ci/cache/target
 export PATH=$CARGO_HOME/bin:$PATH
-mv rust-toolchain-nightly.toml rust-toolchain.toml
 
 cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev 1cc6a2c6d2ec06607499df33e147310095b1afd5
-cargo install --git https://github.com/embassy-rs/docserver --locked --rev a9781633de214ad0d223efbaeeed92e7cb91e2bb
+cargo install --git https://github.com/embassy-rs/docserver --locked --rev 7c16cbd60f8a53d1dc152b3bb51177264042b7fd
+
+mv rust-toolchain-nightly.toml rust-toolchain.toml
 
 cargo embassy-devtool doc -o webroot
 
