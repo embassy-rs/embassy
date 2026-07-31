aa1cfdcbc4b684b00c797cd92553de3f18d5ea88
xoviat <xoviat@users.noreply.github.com>
2026-07-31 10:40:47 -0500

    ci: update docserver and enable monitor

--- .github/ci/doc.sh
+++ .github/ci/doc.sh
@@ -11,12 +11,12 @@ export CARGO_HOME=/ci/cache/cargo
 export CARGO_TARGET_DIR=/ci/cache/target
 export PATH=$CARGO_HOME/bin:$PATH
 
-cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev 1cc6a2c6d2ec06607499df33e147310095b1afd5
-cargo install --git https://github.com/embassy-rs/docserver --locked --rev 545f118568f59f600ff586fbc124fc5f5a3ec88f
+cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev f8a8cce4092ef2566fbae04f088daa70c9e1fe93
+cargo install --git https://github.com/embassy-rs/docserver --locked --rev 2fb83db984a178e5ff24d7b90b2028aeeceab103
 
 mv rust-toolchain-nightly.toml rust-toolchain.toml
 
-cargo embassy-devtool doc -o webroot
+cargo embassy-devtool doc -o webroot --monitor
 
 export KUBECONFIG=/ci/secrets/kubeconfig.yml
 POD=$(kubectl get po -l app=docserver -o jsonpath={.items[0].metadata.name})
