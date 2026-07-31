b904f31e2c8ee096290ba20c476c34f816581c59
xoviat <xoviat@users.noreply.github.com>
2026-07-30 21:06:51 -0500

    ci: revert docserver

--- .github/ci/doc.sh
+++ .github/ci/doc.sh
@@ -2,7 +2,7 @@
 ## on push branch=main
 ## priority -100
 ## dedup dequeue
-## cooldown 3m
+## cooldown 15m
 
 set -euxo pipefail
 
@@ -12,7 +12,7 @@ export CARGO_TARGET_DIR=/ci/cache/target
 export PATH=$CARGO_HOME/bin:$PATH
 
 cargo install --git https://github.com/embassy-rs/cargo-embassy-devtool --locked --rev 1cc6a2c6d2ec06607499df33e147310095b1afd5
-cargo install --git https://github.com/embassy-rs/docserver --locked --rev 7c16cbd60f8a53d1dc152b3bb51177264042b7fd
+cargo install --git https://github.com/embassy-rs/docserver --locked --rev 545f118568f59f600ff586fbc124fc5f5a3ec88f
 
 mv rust-toolchain-nightly.toml rust-toolchain.toml
 
