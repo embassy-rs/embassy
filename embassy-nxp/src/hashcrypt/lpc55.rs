f704d7481e12f7c40a6a74e34dfc81cd259f30a0
VictorVulturescu <victor.vulturescu@gmail.com>
2026-08-24 15:34:52 +0300

    embassy-nxp: add comments for HASHCRYPT SHA-256 driver

--- embassy-nxp/src/hashcrypt/lpc55.rs
+++ embassy-nxp/src/hashcrypt/lpc55.rs
@@ -5,7 +5,7 @@ use pac::syscon::vals::HashAesRst::Released;
 use crate::pac;
 use crate::peripherals::HASHCRYPT;
 
-// Helper function to enable and reset the hash peripheral\
+// Helper function to enable the clock and reset the HASHCRYPT peripheral
 fn enable_reset() {
     pac::SYSCON.ahbclkctrl2().modify(|w| {
         w.set_hash_aes(true);
@@ -15,19 +15,23 @@ fn enable_reset() {
         w.set_hash_aes_rst(Released);
     });
 }
+// The necessary fields for the implementation of SHA-256
 pub struct Sha256<'d> {
-    _peri: Peri<'d, HASHCRYPT>,
-    buffer: [u8; 64],
-    buffer_len: usize,
-    total_len: u64,
+    _peri: Peri<'d, HASHCRYPT>, // The HASHCRYPT peripheral it self, which will be owned by the struct
+    buffer: [u8; 64],           // A 64 byte buffer in which we can dump incoming data streams
+    buffer_len: usize,          // The number of valid bytes currently buffered, resets to 0 after buffer is drained
+    total_len: u64,             // The size of the compleat message to be hashed
 }
 
 impl<'d> Sha256<'d> {
+    // The function which takes ownership of the HASHCRYPT peripheral
+    // and returns an owned value through Self
     pub fn new(peri: Peri<'d, HASHCRYPT>) -> Self {
         enable_reset();
 
         let sha256 = pac::HASHCRYPT;
 
+        // Initialise the HASHCRYPT peripheral in SHA-256 mode
         sha256.ctrl().modify(|w| {
             w.set_mode(Sha2256);
             w.set_new_hash(true);
@@ -40,47 +44,56 @@ impl<'d> Sha256<'d> {
             total_len: 0u64,
         }
     }
-
+    // Accepts an arbitrary-length slice of bytes at the time, to dump in to the buffer untill a full
+    // 64 byte block is build, then drained in to the FIFO
     pub fn update(&mut self, data: &[u8]) {
-        let data_len = data.len() as u32;
-        let sha256 = pac::HASHCRYPT.indata();
-        let mut offset = 0;
-        self.total_len += data.len() as u64;
+        let data_len = data.len() as u32; // Length of the incoming data
+        let sha256 = pac::HASHCRYPT.indata(); // Register which allows us to input data
+        let mut offset = 0; // tracks how many bytes of `data` have been consumed so far
 
+        self.total_len += data.len() as u64;
         while offset < data_len {
-            // how much room do i have in the current word i think ?
+            // how much room is there in the buffer ?
             let space = 64 - self.buffer_len;
 
-            // how many bites can i take from the data ?
+            // how much can i take from the incoming data ?
             let take = space.min((data_len - offset) as usize);
 
-            // move "take" bytes from the data to the buffer, ocupying whatever space is left in that particular word
+            // move "take" bytes from the data to the buffer, occupying whatever space is left in the buffer
             self.buffer[self.buffer_len..(self.buffer_len + take)]
                 .copy_from_slice(&data[(offset as usize)..((offset) as usize) + take]);
 
             self.buffer_len += take;
             offset += take as u32;
 
+            // Once the buffer is full, we drain it in to the FIFO via .indata().set_data()
             if self.buffer_len == 64 {
-                // buffer is full, so we drain the message streamed so far in to the sha2 fifo
+                // buffer is full, so we drain the message streamed so far in to the sha2 FIFO
                 for chunk in self.buffer.chunks_exact(4) {
                     let word = u32::from_le_bytes(chunk.try_into().unwrap());
                     sha256.write(|w| {
                         w.set_data(word);
                     });
                 }
+                // Once the 16 word FIFO is full, hashing begins automatically, and we are free to start
+                // overwriting the buffer so we can fill it once more with the incoming data
                 self.buffer_len = 0;
             }
+
+            // Even though a digest might be ready to read at this point,
+            // polling HASHCRYPT.status().read().digest() here would yield a useless, incomplete hash,
+            // caused by an incomplete message. Instead, we will poll it in [finalize()] when we can
+            // be sure that no more data will be streamed.
         }
     }
-
+    // Returns the final digest and consumes self so it can no longer be called again
     pub fn finalize(mut self) -> [u8; 32] {
         let sha256 = pac::HASHCRYPT.indata();
 
         // Separate the message from the padding with a single 1
         self.buffer[self.buffer_len] = 0x80;
 
-        // add the padding untill the last 2 words
+        // Add the padding until the last 2 words
         for i in (self.buffer_len + 1)..56 {
             self.buffer[i] = 0;
         }
@@ -89,8 +102,8 @@ impl<'d> Sha256<'d> {
         let message_length = self.total_len * 8;
         self.buffer[56..64].copy_from_slice(&message_length.to_be_bytes());
 
-        // now we can hash the final padded block
-
+        // Now we can hash the final padded block
+        // Hashing begins automatically once the 16 words (512 bits) of the FIFO are full
         for chunk in self.buffer.chunks_exact(4) {
             let word = u32::from_le_bytes(chunk.try_into().unwrap());
             sha256.write(|w| {
@@ -98,10 +111,13 @@ impl<'d> Sha256<'d> {
             });
         }
 
-        // Now we can prepair the 8 word digest
+        // Now we can prepare the 8 word digest
         let mut digest: [u8; 32] = [0u8; 32];
         loop {
             let status = pac::HASHCRYPT.status().read().digest();
+            // if status is true, that we have a digest ready to be read. We poll digest here and not in
+            // [update(&mut self, data: &[u8])] since we want to make sure that the entire message has
+            // been streamed.
             if status {
                 for i in 0..8 {
                     let word = pac::HASHCRYPT.digest0(i).read().digest();
@@ -109,6 +125,9 @@ impl<'d> Sha256<'d> {
                 }
                 break;
             }
+            // If status is false, then that means there is no digest ready to be read, and since there is
+            // no more incoming data, then that means we just need to keep waiting and so there is no need
+            // to explicitly handle that case
         }
 
         digest
